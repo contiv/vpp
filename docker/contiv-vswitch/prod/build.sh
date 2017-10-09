7a1c67f2cf0509cbcc35d0342efc0317f12f494d
Rastislav Szabo <raszabo@cisco.com>
2017-10-09 15:15:26 +0200

    fixed all shebangs

--- docker/contiv-vswitch/prod/build.sh
+++ docker/contiv-vswitch/prod/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright (c) 2017 Cisco and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#!/bin/bash
 
 # delete the old prod container if it already exists
 set +e
--- docker/push-all.sh
+++ docker/push-all.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright (c) 2017 Cisco and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#!/bin/bash
 
 # obtain the current git tag for tagging the Docker images
 TAG=`git describe --tags`

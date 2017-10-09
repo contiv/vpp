b3d311d4b4ba50a406ae1bab0655a057f9e9a0db
Rastislav Szabo <raszabo@cisco.com>
2017-10-09 15:08:47 +0200

    fixed CNI

--- docker/contiv-plugins/prod/cni/install.sh
+++ docker/contiv-plugins/prod/cni/install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Copyright (c) 2017 Cisco and/or its affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#!/bin/sh
 
 # The host folders where CNI binaries and config files are located.
 # Defaults can be overridden via environment variables.

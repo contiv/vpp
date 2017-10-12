9a00801bd3add8a87017d01edc375f805b07c4ba
Lukas Macko <lmacko@cisco.com>
2017-10-12 12:16:59 +0200

    Pin to a specific version

--- vendor/github.com/ligato/vpp-agent/vpp/extras/emacs/make-plugin.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/emacs --script
-(load-file "./all-skel.el")
-(make-plugin)
-(save-some-buffers t)
--- vendor/github.com/ligato/vpp-agent/vpp/extras/vagrant/env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export VPP_VAGRANT_DISTRO="ubuntu1404"
-export VPP_VAGRANT_NICS=2
-export VPP_VAGRANT_VMCPU=4
-export VPP_VAGRANT_VMRAM=4096

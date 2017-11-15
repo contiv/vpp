ee4be2b9c11a234f09a1c6248603df51a001a5de
Rastislav Szabo <raszabo@cisco.com>
2017-11-15 10:46:08 +0100

    use specific VPP commit ID 10e7a9f8d8c8572be16f9b3f0395da6f8eff22ec

--- docker/build-all.sh
+++ docker/build-all.sh
@@ -18,7 +18,7 @@ set -e
 
 # default values for build args and VPP commit ID
 export DOCKER_BUILD_ARGS=""
-export VPP_COMMIT_ID="64ae77890c996d03458f0efe084ebb3f69f7e9ff"
+export VPP_COMMIT_ID="10e7a9f8d8c8572be16f9b3f0395da6f8eff22ec"
 
 # override defaults from arguments
 while [ "$1" != "" ]; do

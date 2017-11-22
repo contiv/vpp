127a7705dcb3cf82228622655753240ce909bc73
filip.gschwandtner <filip.gschwandtner@pantheon.tech>
2017-11-22 12:23:03 +0100

    initial changes for supporting compact nesting mapping + multiple parameters extracted to struct

--- cmd/tools/ldpreload-label-injector/main.go
+++ cmd/tools/ldpreload-label-injector/main.go
@@ -13,9 +13,11 @@ import (
 )
 
 const (
-	intendCharacters    = "- "
-	commentCharacters   = "#"
-	defaultIntendLength = 2 //used only when heuristic fails to provide intend lengths used in input file
+	minusIntendCharacter = "-"
+	spaceIntendCharacter = " "
+	intendCharacters     = minusIntendCharacter + spaceIntendCharacter
+	commentCharacters    = "#"
+	defaultIntendLength  = 2 //used only when heuristic fails to provide intend lengths used in input file
 )
 
 var (
@@ -78,7 +80,9 @@ func inject(content string) (string, error) {
 	var converted bytes.Buffer
 	for _, document := range strings.Split(content, "---") {
 		if isPod(document) || isDeployment(document) {
-			converted.WriteString(insertLDPreloadTrue(document, eol))
+			document = insertLDPreloadTrue(document, eol)
+			//document = insertAppScope(document, eol)
+			converted.WriteString(document)
 		} else {
 			converted.WriteString(document)
 		}
@@ -95,7 +99,7 @@ type insertion struct {
 func insertLDPreloadTrue(document string, eol string) string {
 	insertLines := []string{"# ldpreload-related labels", "ldpreload: \"true\""}
 	var insertions = &[]insertion{}
-	visitInsertionPlaces(document, []string{"spec:", "template:", "metadata:", "labels:"}, []string{},
+	visitInsertionPlaces(newTraversingInfo(document, []string{"spec:", "template:", "metadata:", "labels:"},
 		func(index int, unresolvedPath []string, resolvedPath []string, block string, parentBlockIntend int) {
 			intendDelta := defaultIntendLength // just guess in case we don't have enough information to compute it
 			if len(resolvedPath) > 1 {
@@ -116,10 +120,10 @@ func insertLDPreloadTrue(document string, eol string) string {
 				buffer.WriteString(strings.Repeat(" ", parentBlockIntend+intendDelta) + line + eol)
 			}
 
+			//TODO check existence before inserting
 			insertions = prepend(insertion{index, buffer.String()}, insertions) // using this order doesn't invalidate indexes of insertions by applying them sequentially
 			return
-		},
-		0, len(document), eol, 0, 0)
+		}, eol))
 
 	//make real insert
 	for _, insert := range *insertions {
@@ -128,45 +132,130 @@ func insertLDPreloadTrue(document string, eol string) string {
 	return document
 }
 
+func insertAppScope(document string, eol string) string {
+	visitInsertionPlaces(newTraversingInfo(document, []string{"spec:", "template:", "spec:", "containers:", "-", "env:"},
+		func(index int, unresolvedPath []string, resolvedPath []string, block string, parentBlockIntend int) {
+			fmt.Println("========================================")
+			fmt.Print(block)
+			fmt.Println("========================================")
+		}, eol))
+
+	return document
+}
+
 func prepend(item insertion, slice *[]insertion) *[]insertion {
 	newSlice := append([]insertion{item}, *slice...)
 	return &newSlice
 }
 
-func visitInsertionPlaces(doc string, unresolvedPath []string, resolvedPath []string, visitor func(int, []string, []string, string, int), blockStart int, blockEnd int, eol string, parentBlockIntend int, blockIntend int) {
-	curBlock := doc[blockStart:blockEnd]
-	if len(unresolvedPath) == 0 {
-		visitor(strings.Index(curBlock, eol)+len(eol)+blockStart, unresolvedPath, resolvedPath, curBlock, parentBlockIntend)
-		return
-	}
-	matchedBlockPrefixes := regexp.
-		MustCompile(eol+"["+intendCharacters+"]{"+strconv.Itoa(blockIntend)+"}"+unresolvedPath[0]).
-		FindAllStringIndex(curBlock, -1)
-	if len(matchedBlockPrefixes) == 0 { //next block doesn't exist
-		visitor(strings.Index(curBlock, eol)+len(eol)+blockStart, unresolvedPath, resolvedPath, curBlock, parentBlockIntend)
-		return
-	}
-	for _, prefixIndexes := range matchedBlockPrefixes {
-		childBlockStart, childBlockEnd := computeChildBlockPosition(curBlock, prefixIndexes, blockIntend, eol)
-		childBlockIntend := computeChildBlockIntend(curBlock, childBlockStart, childBlockEnd, eol)
-		visitInsertionPlaces(doc, unresolvedPath[1:], append(resolvedPath, unresolvedPath[0]), visitor, blockStart+childBlockStart, blockStart+childBlockEnd, eol, blockIntend, childBlockIntend)
+type traversingInfo struct {
+	// static info that doesn't change by traversing
+	document string
+	visitor  func(int, []string, []string, string, int)
+	eol      string
+
+	// dynamic info changed by traversing
+	unresolvedPath    []string
+	resolvedPath      []string
+	blockStart        int
+	blockEnd          int
+	parentBlockIntend int
+}
+
+func (t *traversingInfo) newDescending(blockStart int, blockEnd int, parentBlockIntend int) traversingInfo {
+	return traversingInfo{
+		document: t.document,
+		visitor:  t.visitor,
+		eol:      t.eol,
+
+		unresolvedPath:    t.unresolvedPath[1:],
+		resolvedPath:      append(t.resolvedPath, t.unresolvedPath[0]),
+		blockStart:        blockStart,
+		blockEnd:          blockEnd,
+		parentBlockIntend: parentBlockIntend,
 	}
 }
-func computeChildBlockIntend(curBlock string, childBlockStart int, childBlockEnd int, eol string) int {
-	nextChildIntendRegExp := eol + "[" + intendCharacters + "]+[^" + intendCharacters + commentCharacters + "]{1}" //TODO convert intendCharacters to regexp? for "- " is it the same
-	nextChildIntend := regexp.MustCompile(nextChildIntendRegExp).FindString(curBlock[childBlockStart:childBlockEnd])
-	if nextChildIntend == "" { //block has no child blocks
+
+func (t *traversingInfo) curBlock() string {
+	return t.document[t.blockStart:t.blockEnd]
+}
+
+func newTraversingInfo(document string, path []string, visitor func(int, []string, []string, string, int), eol string) traversingInfo {
+	return traversingInfo{
+		document:          document,
+		visitor:           visitor,
+		eol:               eol,
+		unresolvedPath:    path,
+		resolvedPath:      []string{},
+		blockStart:        0,
+		blockEnd:          len(document),
+		parentBlockIntend: 0,
+	}
+}
+
+func visitInsertionPlaces(i traversingInfo) {
+	if len(i.unresolvedPath) == 0 {
+		i.visitor(strings.Index(i.curBlock(), i.eol)+len(i.eol)+i.blockStart, i.unresolvedPath, i.resolvedPath, i.curBlock(), i.parentBlockIntend)
+		return
+	}
+
+	if i.unresolvedPath[0] == "-" { // compact nested mapping
+		blockIntend := computeMappingBlockIntend(i.curBlock(), i.eol)
+		mappingItemPrefixes := regexp.
+			MustCompile(i.eol+"["+strconv.Itoa(blockIntend)+"]+"+minusIntendCharacter).
+			FindAllStringIndex(i.curBlock(), -1)
+		for _, prefixIndexes := range mappingItemPrefixes {
+			//itemBlockStart, itemBlockEnd := computeItemBlockPosition(i.curBlock(), prefixIndexes, blockIntend, i.eol)
+			_, _ = computeItemBlockPosition(i.curBlock(), prefixIndexes, blockIntend, i.eol)
+		}
+	} else { // basic blocks
+		blockIntend := computeNormalBlockIntend(i.curBlock(), i.eol)
+		matchedBlockPrefixes := regexp.
+			MustCompile(i.eol+"["+intendCharacters+"]{"+strconv.Itoa(blockIntend)+"}"+i.unresolvedPath[0]).
+			FindAllStringIndex(i.curBlock(), -1)
+		if len(matchedBlockPrefixes) == 0 { //next block doesn't exist
+			i.visitor(strings.Index(i.curBlock(), i.eol)+len(i.eol)+i.blockStart, i.unresolvedPath, i.resolvedPath, i.curBlock(), i.parentBlockIntend)
+			return
+		}
+		for _, prefixIndexes := range matchedBlockPrefixes {
+			childBlockStart, childBlockEnd := computeChildBlockPosition(i.curBlock(), prefixIndexes, blockIntend, i.eol)
+			visitInsertionPlaces(i.newDescending(i.blockStart+childBlockStart, i.blockStart+childBlockEnd, blockIntend))
+		}
+	}
+}
+
+func computeNormalBlockIntend(curBlock string, eol string) int {
+	return computeBlockIntend(curBlock, eol, "[^"+intendCharacters+commentCharacters+"]{1}")
+}
+
+func computeMappingBlockIntend(curBlock string, eol string) int {
+	return computeBlockIntend(curBlock, eol, minusIntendCharacter)
+}
+
+func computeBlockIntend(curBlock string, eol string, lastCharacterRegExp string) int {
+	intendRegExp := eol + "[" + spaceIntendCharacter + "]*" + lastCharacterRegExp //TODO convert intendCharacters to regexp? for "- " is it the same
+	intend := regexp.MustCompile(intendRegExp).FindString(curBlock)
+	if intend == "" { //block has no child blocks
 		return -1
 	}
-	_, lastRuneSize := utf8.DecodeLastRuneInString(nextChildIntend)
-	return len(nextChildIntend) - len(eol) - lastRuneSize //TODO convert byte length to rune length? for " " and "-" it is the same length
+	_, lastRuneSize := utf8.DecodeLastRuneInString(intend)
+	return len(intend) - len(eol) - lastRuneSize //TODO convert byte length to rune length? for " " and "-" it is the same length
 }
+
 func computeChildBlockPosition(curBlock string, childBlockPrefixIndexes []int, blockIntend int, eol string) (int, int) {
-	start := childBlockPrefixIndexes[0] + len(eol)                                                                                         //without eol from previous block
-	nextSibling := eol + "[" + intendCharacters + "]{" + strconv.Itoa(blockIntend) + "}[^" + intendCharacters + commentCharacters + "]{1}" //TODO convert intendCharacters to regexp? for "- " is it the same
-	nextSiblingIdx := regexp.MustCompile(nextSibling).FindStringIndex(curBlock[childBlockPrefixIndexes[0]+len(eol):])                      // shifted search by len(eol) to not match start of block
+	return computeInnerBlockPosition(curBlock, childBlockPrefixIndexes, blockIntend, eol, "[^"+intendCharacters+commentCharacters+"]{1}")
+}
+
+func computeItemBlockPosition(curBlock string, itemBlockPrefixIndexes []int, blockIntend int, eol string) (int, int) {
+	return computeInnerBlockPosition(curBlock, itemBlockPrefixIndexes, blockIntend, eol, minusIntendCharacter)
+}
+
+func computeInnerBlockPosition(curBlock string, innerBlockPrefixIndexes []int, blockIntend int, eol string, lastCharacterRegExp string) (int, int) {
+	start := innerBlockPrefixIndexes[0] + len(eol)                                                                    //without eol from previous block
+	nextSibling := eol + "[" + spaceIntendCharacter + "]{" + strconv.Itoa(blockIntend) + "}" + lastCharacterRegExp    //TODO convert intendCharacters to regexp? for "- " is it the same
+	nextSiblingIdx := regexp.MustCompile(nextSibling).FindStringIndex(curBlock[innerBlockPrefixIndexes[0]+len(eol):]) // shifted search by len(eol) to not match start of block
 	if nextSiblingIdx != nil {
-		return start, childBlockPrefixIndexes[0] + nextSiblingIdx[0] + len(eol) /*shifted sibling search*/ + len(eol) /*include block ending eol (matched by sibling search) */
+		return start, innerBlockPrefixIndexes[0] + nextSiblingIdx[0] + len(eol) /*shifted sibling search*/ + len(eol) /*include block ending eol (matched by sibling search) */
 	}
 	return start, len(curBlock)
 }

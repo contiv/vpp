68d2a8354588caec28472905fd380b5563c20ea1
filip.gschwandtner <filip.gschwandtner@pantheon.tech>
2017-11-24 13:16:47 +0100

    added reading from standard input and writing to standard output changed default output and when help is showed

--- cmd/tools/ldpreload-label-injector/main.go
+++ cmd/tools/ldpreload-label-injector/main.go
@@ -10,10 +10,11 @@ import (
 
 var (
 	// command line flags
-	outputFile    = flag.String("o", "", "Output file override.")
+	inputFile     = flag.String("f", "", "Input file")
+	outputFile    = flag.String("o", "", "Output file")
 	help          = flag.Bool("h", false, "Switch to show help")
 	useDebugLabel = flag.Bool("d", false, "Switch to used debug ldpreload label")
-	proxyName     = flag.String("p", "", "Name of proxy container that should be used.")
+	proxyName     = flag.String("p", "", "Name of proxy container that should be used")
 )
 
 // injectParams pass important information from command line flags to injector
@@ -21,6 +22,17 @@ type injectParams struct {
 	useDebugLabel bool
 	proxyName     string
 }
+
+const helpContent = `ldpreload-label-injector injects ldpreload labels to kubernetes yaml files.
+Usage:
+  ldpreload-label-injector [input file]
+
+Flags:
+  -o [output file]  Sets output for modified kubernetes yaml file. This overrides default behaviour that takes input file as output file and modifies input file in-place.
+  -d                Adds ldpreload debug label to yaml kubernetes files
+  -p                Sets the name of container that should be used as proxy. If not set, proxy is not used.
+  -h                Prints this help
+`
 
 // main is the main method for ldpreload label injector
 // This tool parses block structure of yaml file and currently support only these type of structures:
@@ -37,60 +49,63 @@ type injectParams struct {
 func main() {
 	// handle initial tasks and simple cases
 	flag.Parse() //can't be in init()
-	if *help || flag.NArg() == 0 {
-		printHelp()
+	if *help {
+		fmt.Print(helpContent)
 		return
-	}
-
-	// resolve input/output files arguments, info passing to injector and start file processing
-	inputFile := flag.Arg(0)
-	if len(*outputFile) == 0 {
-		outputFile = &inputFile
 	}
 	injectParams := injectParams{
 		useDebugLabel: *useDebugLabel,
 		proxyName:     *proxyName,
 	}
-	if err := processFile(inputFile, *outputFile, injectParams); err != nil {
-		panic(fmt.Errorf("Can't process file %v : %v ", inputFile, err))
+
+	// transform input to output
+	content, err := readInput()
+	if err != nil {
+		panic(fmt.Errorf("Can't read input: %v ", err))
+	}
+	injectedContent, err := inject(string(content), injectParams)
+	if err != nil {
+		panic(fmt.Errorf("Can't inject ldpreload labels: %v ", err))
+	}
+	err = writeOutput(injectedContent)
+	if err != nil {
+		panic(fmt.Errorf("Can't write output: %v ", err))
 	}
 }
 
-// printHelp prints properly structured help for command line environment
-func printHelp() {
-	fmt.Print(`ldpreload-label-injector injects ldpreload labels to kubernetes yaml files.
-Usage:
-  ldpreload-label-injector [input file]
-
-Flags:
-  -o [output file]  Sets output for modified kubernetes yaml file. This overrides default behaviour that takes input file as output file and modifies input file in-place.
-  -d                Adds ldpreload debug label to yaml kubernetes files
-  -p                Sets the name of container that should be used as proxy. If not set, proxy is not used.
-  -h                Prints this help
-`)
-}
-
-// processFile takes content of input file, injects that the ldpreload labels and outputs it as output file
-func processFile(inputFile string, outputFile string, params injectParams) (err error) {
-	fmt.Printf("Processing file %v (to output %v)... ", inputFile, outputFile)
-	content, err := ioutil.ReadFile(inputFile)
+func readInput() (content string, err error) {
+	if *inputFile == "" {
+		err = fmt.Errorf("Input is not specified, please use -f parameter.\n" + helpContent)
+		return
+	}
+	var contentBytes []byte
+	if *inputFile == "-" {
+		contentBytes, err = ioutil.ReadAll(os.Stdin)
+	} else {
+		contentBytes, err = ioutil.ReadFile(*inputFile)
+	}
 	if err != nil {
 		return
 	}
-	converted, err := inject(string(content), params)
-	if err != nil {
-		return
-	}
-
-	err = ioutil.WriteFile(outputFile, []byte(converted), fileMode(inputFile))
-	fmt.Println("Done")
+	content = string(contentBytes)
 	return
 }
 
+func writeOutput(content string) error {
+	if *outputFile == "" {
+		fmt.Print(content)
+		return nil
+	}
+	return ioutil.WriteFile(*outputFile, []byte(content), fileMode(*inputFile))
+}
+
 // fileMode computes most appropriate file permissions for output file
-func fileMode(inputFile string) os.FileMode {
+func fileMode(input string) os.FileMode {
 	fileMode := os.FileMode(0644) //default permissions
-	inputFileInfo, err := os.Stat(inputFile)
+	if input == "" || input == "-" {
+		return fileMode //input is not file -> can't detect file permissions from input -> using default permissions
+	}
+	inputFileInfo, err := os.Stat(input)
 	if err == nil {
 		fileMode = inputFileInfo.Mode()
 	} else {

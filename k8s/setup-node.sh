edb80498c02d11b112427049d7cccb2a373e639d
Lukas Macko <lmacko@cisco.com>
2017-11-27 14:43:52 +0100

    Installation script

---
+++ k8s/setup-node.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/env bash
+
+#confirm asks a question and based on user input it returns either 1-yes or 0-no.
+confirm () {
+
+   while true
+   do
+    read -r -p "$1 [Y/n] " input
+
+    case $input in
+        [yY][eE][sS]|[yY])
+    echo 1
+    return
+    ;;
+
+        [nN][oO]|[nN])
+    echo 0
+    return
+           ;;
+
+        *)
+    ;;
+    esac
+   done
+}
+
+
+#loads uio_pci_generic driver and setup the loading on each boot up if requested
+installPCIUIO() {
+   sudo modprobe uio_pci_generic
+   if [[ $(confirm "Do you want the PCI UIO driver to be loaded on boot up?") -eq 1 ]]; then
+      # check if the driver is not already added into the file
+      if ! grep -q "uio_pci_generic" /etc/modules; then
+         sudo sh -c "echo uio_pci_generic >> /etc/modules"
+         echo "Module uio_pci_generic was added into /etc/modules"
+      fi
+   fi
+}
+
+#selects an interface that will be used for node interconnect
+selectNodeIntreconnectIf() {
+   interfaces=`sudo lshw -class network -businfo | grep pci`
+   if [[ -z "$interfaces" ]]; then
+      echo "No network devices found."
+      exit 1
+   fi
+
+   echo "The following network devices were found"
+   echo "$interfaces" | sed 's/pci@//g' | awk '{printf "%s) %s %s\n", NR, $2, $1}'
+
+   nicCnt=$(echo "$interfaces" | wc -l)
+
+   selectedDevice=0
+
+   #process input from user
+   while true
+   do
+      read -r -p "Select interface for node interconnect [1-$nicCnt]:" selection
+      case "$selection" in
+         [0-9]*)
+            if [[ "$selection" -gt "$nicCnt" || "$selection" -eq 0 ]]; then
+               echo "Selected option is out of range"
+            else
+               selectedDevice=$(echo "$interfaces" | sed "${selection}q;d")
+               break
+            fi
+            ;;
+         *)
+            echo "Invalid input"
+            ;;
+      esac
+   done
+
+   device=$(echo "$selectedDevice" | awk '{print $2}')
+   pciAddr=$(echo "$selectedDevice" | awk '{print $1}' | sed 's/pci@//g')
+
+   if [[ $(confirm "Device '$device' must be shutdown, do you want to proceed?") -ne 1 ]]; then
+      exit 1
+   fi
+
+   #check whether startup.conf exist
+   startup="
+unix {
+   nodaemon
+   cli-listen 0.0.0.0:5002
+   cli-no-pager
+}
+dpdk {
+   dev $pciAddr
+}
+"
+   echo "$startup"
+
+   if [[ $(confirm "File /etc/vpp/contiv-vswitch.conf will be modified, do you want to proceed?") -ne 1 ]]; then
+      exit 1
+   fi
+
+   #create vpp startup config
+   sudo sh -c "echo '$startup' > /etc/vpp/contiv-vswitch.conf "
+
+   #shutdown interface
+   sudo ip link set "$device" down
+}
+
+## Setup begins
+
+echo "#########################################"
+echo "#   Contiv - VPP                        #"
+echo "#########################################"
+
+if [[ $(confirm "Do you want to setup multinode cluster?") -eq 1 ]]; then
+
+   kernelModule=$(lsmod | grep uio_pci_generic | wc -l)
+   if [[ $kernelModule -gt 0 ]]; then
+         echo "PCI UIO driver is loaded"
+   else
+      echo "PCI UIO driver is not loaded"
+      if [[ $(confirm "Do you want to load PCI UIO driver?") -eq 1 ]]; then
+           installPCIUIO
+      else
+           echo "Unless the driver is loaded, VPP will not be able to grab the NIC."
+           exit 0
+      fi
+
+   fi
+   selectNodeIntreconnectIf
+else
+   echo "Single-node cluster was selected. No changes will be made."
+fi
+
+echo "Configuration of the node finished successfully."

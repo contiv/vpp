510a0f932b9a1409c02b3b478797ee5417d1b4b8
Nikos <nikos.bre@gmail.com>
2017-11-28 13:34:15 +0100

    Bug fix: Return nil or empty array for rules

--- plugins/policy/processor/matches_calculator.go
+++ plugins/policy/processor/matches_calculator.go
@@ -1,19 +1,3 @@
-/*
- * // Copyright (c) 2017 Cisco and/or its affiliates.
- * //
- * // Licensed under the Apache License, Version 2.0 (the "License");
- * // you may not use this file except in compliance with the License.
- * // You may obtain a copy of the License at:
- * //
- * //     http://www.apache.org/licenses/LICENSE-2.0
- * //
- * // Unless required by applicable law or agreed to in writing, software
- * // distributed under the License is distributed on an "AS IS" BASIS,
- * // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * // See the License for the specific language governing permissions and
- * // limitations under the License.
- */
-
 package processor
 
 import (
@@ -41,6 +25,10 @@ func (pp *PolicyProcessor) calculateMatches(policyData *policymodel.Policy) []co
 			ingressIPBlocks := []config.IPBlock{}
 
 			ingressRuleFroms := ingressRule.From
+			if len(ingressRuleFroms) == 0 {
+				ingressPods = nil
+				ingressIPBlocks = nil
+			}
 			for _, ingressRuleFrom := range ingressRuleFroms {
 				ingressLabel := ingressRuleFrom.Pods
 				ingressPod := pp.Cache.LookupPodsByNSLabelSelector(namespace, ingressLabel)
@@ -98,6 +86,10 @@ func (pp *PolicyProcessor) calculateMatches(policyData *policymodel.Policy) []co
 			egressIPBlocks := []config.IPBlock{}
 
 			egressRulesTo := egressRule.To
+			if len(egressRulesTo) == 0 {
+				egressPods = nil
+				egressIPBlocks = nil
+			}
 			for _, egressRuleTo := range egressRulesTo {
 				egressLabel := egressRuleTo.Pods
 				egressPod := pp.Cache.LookupPodsByNSLabelSelector(namespace, egressLabel)

cffdd3c3a2f1605c946870602b5bb8832cc2c2fd
Vratko Polak <vrpolak@cisco.com>
2017-11-30 19:51:11 +0100

    Issue #354: Squashed commits from dev branch
    Docker image tagc sontain branch name (if not master).
    Ability to delete images after upload to save space on build machine.
    TAG and VPP values are experted and consumed by robot.
    When non-empty, the values make robot apply specifier tags.
    Robot-edited files are left in results folder for archivation.
    
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- docker/push-all.sh
+++ docker/push-all.sh
@@ -20,6 +20,7 @@ set -e
 BRANCH_NAME="master"
 SKIP_UPLOAD="false"
 DEV_UPLOAD="false"
+CLEANUP="false"
 
 # list of images we are tagging & pushing
 IMAGES=("cni" "ksr" "cri")
@@ -34,14 +35,16 @@ while [ "$1" != "" ]; do
             echo "Using branch name: ${BRANCH_NAME}"
             ;;
         -s | --skip-upload )
-            shift
             SKIP_UPLOAD="true"
             echo "Using skip upload: ${SKIP_UPLOAD}"
             ;;
         -d | --dev-upload )
-            shift
             DEV_UPLOAD="true"
             echo "Using dev upload: ${DEV_UPLOAD}"
+            ;;
+        -c | --cleanup )
+            CLEANUP="true"
+            echo "Using cleanup: ${CLEANUP}"
             ;;
         * )
             echo "Invalid parameter: "$1
@@ -51,8 +54,10 @@ while [ "$1" != "" ]; do
 done
 
 # obtain the current git tag for tagging the Docker images
-TAG=`git describe --tags`
-VPP=$(docker run --rm dev-contiv-vswitch:$TAG bash -c "cd \$VPP_DIR && git rev-parse --short HEAD")
+export TAG=`git describe --tags`
+echo "exported TAG=$TAG"
+export VPP=$(docker run --rm dev-contiv-vswitch:$TAG bash -c "cd \$VPP_DIR && git rev-parse --short HEAD")
+echo "exported VPP=$VPP"
 
 # tag and push each image
 for IMAGE in "${IMAGES[@]}"
@@ -72,12 +77,14 @@ do
         fi
     else
         # other branch - tag with the branch name
-        echo "Tagging as contivvpp/${IMAGE}:${BRANCH_NAME}"
+        echo "Tagging as contivvpp/${IMAGE}:${BRANCH_NAME}-${TAG} + contivvpp/${IMAGE}:${BRANCH_NAME}"
         sudo docker tag prod-contiv-${IMAGE}:${TAG} contivvpp/${IMAGE}:${BRANCH_NAME}
+        sudo docker tag prod-contiv-${IMAGE}:${TAG} contivvpp/${IMAGE}:${BRANCH_NAME}-${TAG}
 
         # push the images
         if [ "${SKIP_UPLOAD}" != "true" ]
         then
+            sudo docker push contivvpp/${IMAGE}:${BRANCH_NAME}-${TAG}
             sudo docker push contivvpp/${IMAGE}:${BRANCH_NAME}
         fi
     fi
@@ -100,13 +107,15 @@ do
         fi
     else
         # other branch - tag with the branch name
-        echo "Tagging as contivvpp/${IMAGE}:${BRANCH_NAME}"
+        echo "Tagging as contivvpp/${IMAGE}:${BRANCH_NAME}-${TAG}-${VPP} + contivvpp/${IMAGE}:${BRANCH_NAME}"
         sudo docker tag prod-contiv-${IMAGE}:${TAG} contivvpp/${IMAGE}:${BRANCH_NAME}
+        sudo docker tag prod-contiv-${IMAGE}:${TAG} contivvpp/${IMAGE}:${BRANCH_NAME}-${TAG}-${VPP}
 
         # push the images
         if [ "${SKIP_UPLOAD}" != "true" ]
         then
             sudo docker push contivvpp/${IMAGE}:${BRANCH_NAME}
+            sudo docker push contivvpp/${IMAGE}:${BRANCH_NAME}-${TAG}-${VPP}
         fi
     fi
 done
@@ -116,7 +125,7 @@ then
     if [ "${BRANCH_NAME}" == "master" ]
     then
         # master branch - tag with the git tag + "latest"
-        echo "Tagging as contivvpp/dev-contiv-vswitch:${TAG}-${VPP} + contivvpp/dev-vswitch:latest"
+        echo "Tagging as contivvpp/dev-vswitch:${TAG}-${VPP} + contivvpp/dev-vswitch:latest"
         sudo docker tag dev-contiv-vswitch:${TAG} contivvpp/dev-vswitch:${TAG}-${VPP}
         sudo docker tag dev-contiv-vswitch:${TAG} contivvpp/dev-vswitch:latest
 
@@ -128,13 +137,20 @@ then
         fi
     else
         # other branch - tag with the branch name
-        echo "Tagging as contivvpp/dev-vswitch:${BRANCH_NAME}"
+        echo "Tagging as contivvpp/dev-vswitch:${BRANCH_NAME}-${TAG}-${VPP} + contivvpp/dev-vswitch:${BRANCH_NAME}"
         sudo docker tag dev-contiv-vswitch:${TAG} contivvpp/dev-vswitch:${BRANCH_NAME}
+        sudo docker tag dev-contiv-vswitch:${TAG} contivvpp/dev-vswitch:${BRANCH_NAME}-${TAG}-${VPP}
 
         # push the images
         if [ "${SKIP_UPLOAD}" != "true" ]
         then
             sudo docker push contivvpp/dev-vswitch:${BRANCH_NAME}
+            sudo docker push contivvpp/dev-vswitch:${BRANCH_NAME}-${TAG}-${VPP}
         fi
     fi
+fi
+
+if [ "${CLEANUP}" == "true" ]
+then
+    sudo docker images | fgrep "${TAG}" | awk '{print $3}' | sort -u | xargs sudo docker rmi -f || true
 fi

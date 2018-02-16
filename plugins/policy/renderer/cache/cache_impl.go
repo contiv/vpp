825e88a0c1651b5dff48ceedd75047fc11aeb32b
Milan Lenco <milan.lenco@pantheon.tech>
2018-02-16 16:03:03 +0100

    RendererCache: implemented Resync, Commit, Changes and all getters
    Update() method is still to-be-done.

--- plugins/policy/renderer/cache/cache_impl.go
+++ plugins/policy/renderer/cache/cache_impl.go
@@ -17,6 +17,8 @@
 package cache
 
 import (
+	"crypto/rand"
+	"fmt"
 	"net"
 
 	"github.com/ligato/cn-infra/logging"
@@ -28,6 +30,14 @@ import (
 // RendererCache implements RendererCacheAPI.
 type RendererCache struct {
 	Deps
+
+	// tables
+	localTables       *LocalTables
+	globalTable       *ContivRuleTable
+	allocatedTableIDs AllocatedIDs
+
+	// last received pod configuration
+	config Config
 }
 
 // Deps lists dependencies of RendererCache.
@@ -38,7 +48,28 @@ type Deps struct {
 // RendererCacheTxn represents a single transaction of RendererCache.
 type RendererCacheTxn struct {
 	cache *RendererCache
+
+	// tables with changes from the transaction.
+	localTables *LocalTables
+	globalTable *ContivRuleTable
+
+	// updated pod configuration
+	config Config
 }
+
+// PodConfig encapsulates pod config (as received through RendererCacheTxn.Update()).
+type PodConfig struct {
+	podIP   *net.IPNet
+	ingress []*renderer.ContivRule
+	egress  []*renderer.ContivRule
+}
+
+// Config is used to store snapshot of the configuration as received through
+// RendererCacheTxn.Update().
+type Config map[podmodel.ID]PodConfig
+
+// AllocatedIDs represents a set of all allocated IDs.
+type AllocatedIDs map[string]struct{}
 
 /*********************************** CACHE ***********************************/
 
@@ -46,105 +77,271 @@ type RendererCacheTxn struct {
 // The caller selects the orientation of the traffic at which the rules are applied
 // in the destination network stack.
 func (rc *RendererCache) Init(orientation CacheOrientation) error {
-	/* TODO */
+	rc.localTables = NewLocalTables(rc.Log)
+	rc.globalTable = NewContivRuleTable(GlobalTableID)
+	rc.allocatedTableIDs = make(AllocatedIDs)
+	rc.config = make(Config)
 	return nil
 }
 
 // NewTxn starts a new transaction. The changes are reflected in the cache
 // only after Commit() is called.
 func (rc *RendererCache) NewTxn() Txn {
-	/* TODO */
-	return nil
+	return &RendererCacheTxn{
+		cache:       rc,
+		localTables: NewLocalTables(rc.Log),
+		globalTable: nil,
+		config:      make(Config),
+	}
 }
 
 // Resync completely replaces the existing cache content with the supplied
 // data.
 func (rc *RendererCache) Resync(tables []*ContivRuleTable) error {
-	/* TODO */
+
+	// Re-synchronize outside of the cache first.
+	// In-progress failure should not affect the cache content.
+	allocatedTableIDs := make(AllocatedIDs)
+	localTables := NewLocalTables(rc.Log)
+	globalTable := NewContivRuleTable(GlobalTableID)
+
+	// Build the list of local tables.
+	for _, table := range tables {
+		if table == nil {
+			continue
+		}
+		if table.Type == Global {
+			globalTable = table
+			continue
+		}
+		if len(table.Pods) == 0 {
+			// Skip unused local tables.
+			continue
+		}
+		_, duplicity := allocatedTableIDs[table.ID]
+		if duplicity {
+			return fmt.Errorf("duplicate ContivRuleTable ID: %s", table.ID)
+		}
+		allocatedTableIDs[table.ID] = struct{}{}
+		localTables.Insert(table)
+	}
+
+	// Replace the cache content.
+	rc.allocatedTableIDs = allocatedTableIDs
+	rc.localTables = localTables
+	rc.globalTable = globalTable
 	return nil
 }
 
 // GetPodConfig returns the current configuration of a given pod
-// (as passed through the Txn.Update() method).
+// (as passed through the Txn.Update() method the last time).
 func (rc *RendererCache) GetPodConfig(pod podmodel.ID) (podIP *net.IPNet, ingress, egress []*renderer.ContivRule) {
-	/* TODO */
-	return nil, nil, nil
+	config, hasConfig := rc.config[pod]
+	if !hasConfig {
+		return nil, ingress, egress
+	}
+	return config.podIP, config.ingress, config.egress
 }
 
 // IsolatedPods returns the set of IDs of all pods that have a local table assigned.
 // The term "isolated" is borrowed from K8s, pods become isolated by having
 // a NetworkPolicy that selects them.
 func (rc *RendererCache) IsolatedPods() PodSet {
-	/* TODO */
-	return nil
+	return rc.localTables.IsolatedPods()
 }
 
 // GetLocalTableByPod returns the local table assigned to a given pod.
 // Returns nil if the pod has no table assigned (non-isolated).
 func (rc *RendererCache) GetLocalTableByPod(pod podmodel.ID) *ContivRuleTable {
-	/* TODO */
-	return nil
+	table := rc.localTables.LookupByPod(pod)
+	if table != nil && table.NumOfRules == 0 {
+		table = nil /* do not return empty table */
+	}
+	return table
 }
 
 // GetGlobalTable returns the global table.
 // The function never returns nil but may return table with empty set of rules
 // (meaning ALLOW-ALL).
 func (rc *RendererCache) GetGlobalTable() *ContivRuleTable {
-	/* TODO */
-	return nil
+	return rc.globalTable
 }
 
 /************************************ TXN ************************************/
 
 func (rct *RendererCacheTxn) Update(pod podmodel.ID, podIP *net.IPNet, ingress []*renderer.ContivRule, egress []*renderer.ContivRule) {
-	/* TODO */
+	rct.config[pod] = PodConfig{podIP: podIP, ingress: ingress, egress: egress}
+
+	/* TODO: refresh local tables + global table in the transaction */
 }
 
 // UpdatedPods returns the set of all pods included in the transaction.
 func (rct *RendererCacheTxn) UpdatedPods() PodSet {
-	/* TODO */
-	return nil
+	updated := NewPodSet()
+	for pod := range rct.config {
+		updated.Add(pod)
+	}
+	return updated
 }
 
 // Changes calculates a minimalistic set of changes prepared in the transaction
 // up to this point.
 // Must be run before Commit().
 func (rct *RendererCacheTxn) Changes() (changes []*TxnChange) {
-	/* TODO */
-	return nil
+	// Get changes related to local tables.
+	for i := 0; i < rct.localTables.numTables; i++ {
+		txnTable := rct.localTables.tables[i]
+		origTable := rct.cache.localTables.LookupByID(txnTable.ID)
+		if txnTable.NumOfRules == 0 {
+			// skip empty local tables
+			continue
+		}
+		if len(txnTable.Pods) == 0 && origTable == nil {
+			// added and removed in the same transaction => skip
+			continue
+		}
+		if origTable != nil && txnTable.Pods.Equals(origTable.Pods) {
+			// nothing has really changed for this table
+			continue
+		}
+		change := &TxnChange{
+			Table: txnTable,
+		}
+		if origTable != nil {
+			change.PreviousPods = origTable.Pods.Copy()
+		} else {
+			change.PreviousPods = NewPodSet()
+		}
+		changes = append(changes, change)
+	}
+
+	// Check if the global table has changed.
+	if rct.globalTable != nil &&
+		compareRuleLists(
+			rct.globalTable.Rules[:rct.globalTable.NumOfRules],
+			rct.cache.globalTable.Rules[:rct.cache.globalTable.NumOfRules]) != 0 {
+
+		change := &TxnChange{
+			Table: rct.globalTable,
+		}
+		changes = append(changes, change)
+	}
+
+	return changes
 }
 
 // Commit applies the changes into the underlying cache.
 func (rct *RendererCacheTxn) Commit() error {
-	/* TODO */
+	// Commit local tables.
+	for i := 0; i < rct.localTables.numTables; i++ {
+		txnTable := rct.localTables.tables[i]
+		origTable := rct.cache.localTables.LookupByID(txnTable.ID)
+
+		if origTable != nil {
+			if len(txnTable.Pods) == 0 {
+				// Local table removed in the transaction.
+				rct.cache.localTables.Remove(txnTable)
+				rct.cache.Log.WithFields(logging.Fields{
+					"table": txnTable,
+				}).Debug("Local table was removed in the transaction")
+			} else if !txnTable.Pods.Equals(origTable.Pods) {
+				rct.cache.Log.WithFields(logging.Fields{
+					"table":    txnTable,
+					"origPods": origTable.Pods,
+					"newPods":  txnTable.Pods,
+				}).Debug("Local table was re-assigned to different set of pods in the transaction")
+				// Update interfaces.
+				for pod := range origTable.Pods {
+					if !txnTable.Pods.Has(pod) {
+						rct.cache.localTables.UnassignPod(origTable, pod)
+					}
+				}
+				for pod := range txnTable.Pods {
+					if !origTable.Pods.Has(pod) {
+						rct.cache.localTables.AssignPod(origTable, pod)
+					}
+				}
+				// Copy Private which may have been changed by the cache user.
+				origTable.Private = txnTable.Private
+			}
+		} else {
+			if len(txnTable.Pods) != 0 {
+				// New local table created in the transaction.
+				rct.cache.localTables.Insert(txnTable)
+				rct.cache.Log.WithFields(logging.Fields{
+					"table": txnTable,
+				}).Debug("New local table was created in the transaction")
+			}
+		}
+	}
+
+	// Commit global table.
+	if rct.globalTable != nil {
+		rct.cache.globalTable = rct.globalTable
+	}
 	return nil
 }
 
 // GetPodConfig returns the configuration of a given pod pending in the transaction
 // (applied after the commit).
 func (rct *RendererCacheTxn) GetPodConfig(pod podmodel.ID) (podIP *net.IPNet, ingress, egress []*renderer.ContivRule) {
-	/* TODO */
-	return nil, nil, nil
+	config, hasConfig := rct.config[pod]
+	if !hasConfig {
+		return nil, ingress, egress
+	}
+	return config.podIP, config.ingress, config.egress
 }
 
 // IsolatedPods returns the set of IDs of pods that will have a local table assigned
 // if the transaction is committed without any additional changes.
 func (rct *RendererCacheTxn) IsolatedPods() PodSet {
-	/* TODO */
-	return nil
+	isolated := rct.localTables.IsolatedPods()
+	// Add isolated pods that are without changes in the transaction.
+	for pod := range rct.cache.IsolatedPods() {
+		if rct.localTables.LookupByPod(pod) == nil {
+			isolated.Add(pod)
+		}
+	}
+	return isolated
 }
 
 // GetLocalTableByPod returns the local table that will be assigned to a given pod
 // if the transaction is committed without any additional changes.
 // Returns nil if the pod will be non-isolated.
 func (rct *RendererCacheTxn) GetLocalTableByPod(pod podmodel.ID) *ContivRuleTable {
-	/* TODO */
-	return nil
+	table := rct.localTables.LookupByPod(pod)
+	if table != nil && table.NumOfRules == 0 {
+		return nil /* do not return empty table */
+	}
+	if table != nil {
+		return table
+	}
+	// table == nil => no change in the transaction
+	return rct.cache.GetLocalTableByPod(pod)
 }
 
 // GetGlobalTable returns the global table that will be installed if the transaction
 // is committed without any additional changes
 func (rct *RendererCacheTxn) GetGlobalTable() *ContivRuleTable {
-	/* TODO */
-	return nil
+	if rct.globalTable != nil {
+		return rct.globalTable
+	}
+	// globalTable == nil => no change in the transaction
+	return rct.cache.globalTable
+}
+
+// Generator for ContivRuleTable IDs.
+func (rct *RendererCacheTxn) generateListID() string {
+	var id string
+	for {
+		// Generate random suffix, 10 characters long.
+		b := make([]byte, 5)
+		rand.Read(b)
+		id = fmt.Sprintf("%X", b)
+		if _, exists := rct.cache.allocatedTableIDs[id]; !exists {
+			rct.cache.allocatedTableIDs[id] = struct{}{}
+			break
+		}
+	}
+	return id
 }
---
+++ plugins/policy/renderer/cache/local_tables.go
@@ -0,0 +1,211 @@
+package cache
+
+import (
+	"sort"
+
+	"github.com/ligato/cn-infra/logging"
+
+	podmodel "github.com/contiv/vpp/plugins/ksr/model/pod"
+	"github.com/contiv/vpp/plugins/policy/renderer"
+	"github.com/contiv/vpp/plugins/policy/utils"
+)
+
+// LocalTables is an ordered list of all cached local tables.
+// It has efficient operations: apart from Remove() and RemoveByPredicate(),
+// all with logarithmic or constant complexity.
+//
+// API:
+//  Insert(table)
+//  Remove(table)
+//  RemoveByIdx(idx)
+//  RemoveByPredicate(func(table) -> bool)
+//  LookupByID(ID) -> table
+//  LookupByRules() -> table
+//  LookupByPod(pod) -> table
+//  AssignPod(table, podID)
+//  UnassignPod(table/nil=all, podID)
+type LocalTables struct {
+	Log       logging.Logger
+	tables    []*ContivRuleTable               /* ordered by rules */
+	numTables int                              /* actual number of tables, the rest are nils */
+	byID      map[string]*ContivRuleTable      /* search by table ID */
+	byPod     map[podmodel.ID]*ContivRuleTable /* search local table associated to a given pod */
+}
+
+// NewLocalTables is a constructor for LocalTables.
+func NewLocalTables(logger logging.Logger) *LocalTables {
+	return &LocalTables{
+		Log:    logger,
+		tables: make([]*ContivRuleTable, 0, 100),
+		byID:   make(map[string]*ContivRuleTable),
+		byPod:  make(map[podmodel.ID]*ContivRuleTable),
+	}
+}
+
+// Insert local table into the list.
+func (lts *LocalTables) Insert(table *ContivRuleTable) bool {
+	_, alreadyAdded := lts.byID[table.ID]
+	if alreadyAdded {
+		return false
+	}
+
+	// Insert the table at the right index to keep the order.
+	tableIdx := lts.lookupIdxByRules(table.Rules)
+	if lts.numTables == len(lts.tables) {
+		/* just increase the size by one */
+		lts.tables = append(lts.tables, nil)
+	}
+	if tableIdx < lts.numTables {
+		copy(lts.tables[tableIdx+1:], lts.tables[tableIdx:])
+	}
+	lts.tables[tableIdx] = table
+	lts.numTables++
+	lts.byID[table.ID] = table
+	for pod := range table.Pods {
+		lts.UnassignPod(nil, pod)
+		lts.byPod[pod] = table
+	}
+	return true
+}
+
+// Remove local table from the list (by pointer).
+func (lts *LocalTables) Remove(table *ContivRuleTable) bool {
+	for i := 0; i < lts.numTables; i++ {
+		if table == lts.tables[i] {
+			return lts.RemoveByIdx(i)
+		}
+	}
+	return false
+}
+
+// RemoveByIdx removes local table under a given index from the list.
+// Returns *true* if the index is valid and the table was removed.
+func (lts *LocalTables) RemoveByIdx(idx int) bool {
+	if idx < lts.numTables {
+		table := lts.tables[idx]
+		if idx < lts.numTables-1 {
+			copy(lts.tables[idx:], lts.tables[idx+1:])
+		}
+		lts.numTables--
+		lts.tables[lts.numTables] = nil
+		delete(lts.byID, table.ID)
+		for pod := range table.Pods {
+			delete(lts.byPod, pod)
+		}
+		return true
+	}
+	return false
+}
+
+// RemoveByPredicate removes local table that satisfies a given predicate.
+func (lts *LocalTables) RemoveByPredicate(predicate func(table *ContivRuleTable) bool) int {
+	tableIdx := 0
+	count := 0
+	for tableIdx < lts.numTables {
+		if predicate(lts.tables[tableIdx]) == true {
+			lts.RemoveByIdx(tableIdx)
+			count++
+		} else {
+			tableIdx++
+		}
+	}
+	return count
+}
+
+// AssignPod creates association between the pod and the table.
+func (lts *LocalTables) AssignPod(table *ContivRuleTable, podID podmodel.ID) {
+	lts.UnassignPod(nil, podID)
+	table.Pods.Add(podID)
+	lts.byPod[podID] = table
+}
+
+// UnassignPod removes association between the pod and the table.
+// <table> may be nil to match any local table.
+func (lts *LocalTables) UnassignPod(table *ContivRuleTable, podID podmodel.ID) {
+	if table != nil {
+		table.Pods.Remove(podID)
+	}
+	if table2, assigned := lts.byPod[podID]; assigned {
+		if table == nil || table == table2 {
+			table2.Pods.Remove(podID)
+			delete(lts.byPod, podID)
+		}
+	}
+}
+
+// LookupByID searches for table by ID.
+func (lts *LocalTables) LookupByID(id string) *ContivRuleTable {
+	table, exists := lts.byID[id]
+	if exists {
+		return table
+	}
+	return nil
+}
+
+// LookupByRules searches for table by rules.
+// If there are multiple tables with this list of rules, the one with the smallest
+// index in the list of tables is returned.
+func (lts *LocalTables) LookupByRules(rules []*renderer.ContivRule) *ContivRuleTable {
+	tableIdx := lts.lookupIdxByRules(rules)
+	if tableIdx < lts.numTables &&
+		compareRuleLists(rules, lts.tables[tableIdx].Rules) == 0 {
+		return lts.tables[tableIdx]
+	}
+	return nil
+}
+
+// LookupByPod searches for table by an assigned pod.
+func (lts *LocalTables) LookupByPod(podID podmodel.ID) *ContivRuleTable {
+	table, exists := lts.byPod[podID]
+	if exists {
+		return table
+	}
+	return nil
+}
+
+// IsolatedPods returns the set of IDs of all pods that have a (non-empty) local table assigned.
+// The term "isolated" is borrowed from K8s, pods become isolated by having
+// a NetworkPolicy that selects them.
+func (lts *LocalTables) IsolatedPods() PodSet {
+	pods := NewPodSet()
+	for podID, table := range lts.byPod {
+		if table.NumOfRules > 0 {
+			pods.Add(podID)
+		}
+	}
+	return pods
+}
+
+// lookupIdxByRules returns index in the list of local tables, where a table
+// with given rules is/should be to respect the order.
+func (lts *LocalTables) lookupIdxByRules(rules []*renderer.ContivRule) int {
+	return sort.Search(lts.numTables,
+		func(i int) bool {
+			return compareRuleLists(rules, lts.tables[i].Rules) <= 0
+		})
+}
+
+// compareRuleLists returns an integer comparing two lists of Contiv rules
+// lexicographically.
+func compareRuleLists(a, b []*renderer.ContivRule) int {
+	if a == nil && b == nil {
+		return 0
+	}
+	if a == nil {
+		return -1
+	}
+	if b == nil {
+		return 1
+	}
+	lenOrder := utils.CompareInts(len(a), len(b))
+	if lenOrder != 0 {
+		return lenOrder
+	}
+	for i := range a {
+		ruleOrder := a[i].Compare(b[i])
+		if ruleOrder != 0 {
+			return ruleOrder
+		}
+	}
+	return 0
+}

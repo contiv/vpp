b7d15962a2381eeac152d72cd7069866642701eb
Milan Lenco <milan.lenco@pantheon.tech>
2018-03-06 09:36:08 +0100

    Expose NodePort Services also on the internal (host) node IP.

--- plugins/service/processor/data_resync.go
+++ plugins/service/processor/data_resync.go
@@ -18,11 +18,13 @@ package processor
 
 import (
 	"fmt"
+	"net"
 
 	"github.com/ligato/cn-infra/datasync"
 	"github.com/ligato/cn-infra/logging"
 
 	epmodel "github.com/contiv/vpp/plugins/ksr/model/endpoints"
+	nodemodel "github.com/contiv/vpp/plugins/ksr/model/node"
 	podmodel "github.com/contiv/vpp/plugins/ksr/model/pod"
 	svcmodel "github.com/contiv/vpp/plugins/ksr/model/service"
 )
@@ -30,9 +32,10 @@ import (
 // ResyncEventData wraps an entire state of K8s services that should be reflected
 // into VPP.
 type ResyncEventData struct {
-	Pods      []*podmodel.Pod
-	Endpoints []*epmodel.Endpoints
-	Services  []*svcmodel.Service
+	NodeInternalIP net.IP
+	Pods           []*podmodel.Pod
+	Endpoints      []*epmodel.Endpoints
+	Services       []*svcmodel.Service
 }
 
 // NewResyncEventData creates an empty instance of ResyncEventData.
@@ -67,8 +70,8 @@ func (red ResyncEventData) String() string {
 			services += ", "
 		}
 	}
-	return fmt.Sprintf("ResyncEventData <Pods:[%s] Endpoint:[%s] Services:[%s]>",
-		pods, endpoints, services)
+	return fmt.Sprintf("ResyncEventData <NodeInternalIP:%s Pods:[%s] Endpoint:[%s] Services:[%s]>",
+		red.NodeInternalIP.String(), pods, endpoints, services)
 }
 
 func (sc *ServiceProcessor) parseResyncEv(resyncEv datasync.ResyncEvent) *ResyncEventData {
@@ -127,13 +130,25 @@ func (sc *ServiceProcessor) parseResyncEv(resyncEv datasync.ResyncEvent) *Resync
 				}
 				continue
 			}
+
+			// Parse node RESYNC event
+			nodeName, err := nodemodel.ParseNodeFromKey(key)
+			if err == nil && nodeName == sc.ServiceLabel.GetAgentLabel() {
+				value := &nodemodel.Node{}
+				err := evData.GetValue(value)
+				if err == nil {
+					event.NodeInternalIP = sc.getNodeInternalIP(value)
+				}
+				continue
+			}
 		}
 	}
 
 	sc.Log.WithFields(logging.Fields{
-		"num-pods":      numPod,
-		"num-endpoints": numEps,
-		"num-services":  numSvc,
+		"num-pods":         numPod,
+		"num-endpoints":    numEps,
+		"num-services":     numSvc,
+		"node-internal-IP": event.NodeInternalIP,
 	}).Debug("Parsed RESYNC event")
 
 	return event

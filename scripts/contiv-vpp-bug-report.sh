0925933ab9dfca8efa61f222cdfd91d387e36c83
Jan Medved <jmedved@cisco.com>
2018-03-21 11:30:45 -0700

    Add argument checking

--- scripts/contiv-vpp-bug-report.sh
+++ scripts/contiv-vpp-bug-report.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 
 #######################################################
 # Example Usage
@@ -7,24 +7,45 @@
 
 set -euo pipefail
 
+num_args=$#
+
+if [ $num_args -lt 1 -o  $num_args -gt 2 ] ; then
+  echo "Usage: $0 <k8s-master-IP-address> [<username>]"
+  echo ""
+  echo "k8s-master-IP-address: IP address of k8s master node from which to get"
+  echo "          the debug data"
+  echo "username: optional username to login into the k8s master node. If no user"
+  echo "          name is specified, the current user will be used. The user must"
+  echo "          have passwordless access to the k8s master node and must be able"
+  echo "          to execute passwordless sudo."
+  echo ""
+  exit 1
+fi
+
+master=$1
+if [ $num_args -eq 2 ] ; then
+  user=$2
+else
+  user=$(whoami)
+fi
+
 stamp="$(date "+%Y-%m-%d-%H-%M")"
 report_dir="contiv-vpp-bugreport-$stamp"
 mkdir -p $report_dir
-user=$1
-master=$2
 
 vswitch_pods="$(ssh $user@$master kubectl get po -n kube-system -l k8s-app=contiv-vswitch -o go-template=\'{{range .items}}{{printf \"%s,%s \" \(index .metadata\).name \(index .spec\).nodeName}}{{end}}\')"
 for p in $vswitch_pods; do
   IFS=',' read -r -a fields <<< "$p"
-  echo "Collecting logs for vswitch" \'${fields[0]}\'  "on node" \'${fields[1]}\'
+  echo "Collecting logs for vswitch" \'${fields[0]}\'  "on node" \'${fields[1]}\'...
   mkdir $report_dir/${fields[1]}
   ssh $user@$master kubectl logs ${fields[0]} -n kube-system -c contiv-vswitch > $report_dir/${fields[1]}/${fields[0]}.log
 done
 
 ssh $user@$master kubectl describe configmaps -n kube-system contiv-agent-cfg > $report_dir/vpp.yaml
-ssh $user@$master kubectl get nodes -o wide >  $report_dir/nodes.txt
-ssh $user@$master kubectl get pods -o wide --all-namespaces > $report_dir/pods.txt
-ssh $user@$master kubectl get services -o wide --all-namespaces > $report_dir/services.txt
+ssh $user@$master kubectl get nodes -o wide >  $report_dir/k8s-nodes.txt
+ssh $user@$master kubectl get pods -o wide --all-namespaces > $report_dir/k8s-pods.txt
+ssh $user@$master kubectl get services -o wide --all-namespaces > $report_dir/k8s-services.txt
+ssh $user@$master kubectl get networkpolicy -o wide --all-namespaces > $report_dir/k8s-policies.txt
 
 nodes="$(ssh $user@$master kubectl get nodes -o go-template=\'{{range .items}}{{printf \"%s,%s \" \(index .status.addresses 0\).address \(index .status.addresses 1\).address}}{{end}}\')"
 for n in $nodes; do

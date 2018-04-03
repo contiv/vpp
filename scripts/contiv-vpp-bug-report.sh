1015ab56a7f365ae171f34bd4a1a7c69dd931bb7
Jan Medved <jmedved@cisco.com>
2018-04-03 07:44:12 -0700

    Add support for collecting data from vagrant test beds

--- scripts/contiv-vpp-bug-report.sh
+++ scripts/contiv-vpp-bug-report.sh
@@ -7,49 +7,87 @@
 # <user-id>:             User id used to login to the k8s master
 #################################################################
 
+set -euo pipefail
+
 get_vpp_data() {
-  echo " . $2"
-  ssh -t "$user"@"${fields[0]}" echo $1 \| sudo nc -U /run/vpp/cli.sock \> /tmp/$report_dir/vpp-$2.txt 2>/dev/null
+  echo "    . $2"
+  ssh -t $SSH_OPTS "$SSH_USER"@"$NODE" echo $1 \| sudo nc -U /run/vpp/cli.sock \> /tmp/$REPORT_DIR/vpp-$2.txt 2>/dev/null
 }
 
-set -euo pipefail
+usage() {
+  echo ""
+  echo "usage: $0 -m <k8s-master-node> [-u <user>] "
+  echo "        [-f <ssh-config-file>]"
+  echo ""
+  echo "<k8s-master-node>: IP address or name of the k8s master node from"
+  echo "        which to retrieve the debug data."
+  echo "<user>: optional username to login into the k8s master node. If"
+  echo "        no username is specified, the current user will be used."
+  echo "        The user must have passwordless access to the k8s master"
+  echo "        node, and must be able to execute passwordless sudo. If"
+  echo "        logging into a node created by vagrant, specify username"
+  echo "        vagrant'."
+  echo "ssh-config-file: optional path to ssh configuration file. The ssh"
+  echo "        configuration file must be specified when logging into"
+  echo "        vagrant nodes."
+}
 
 num_args=$#
 
-if [ "$num_args" -lt 1 -o  "$num_args" -gt 2 ] ; then
-  echo "Usage: $0 <k8s-master-IP-address> [<username>]"
-  echo ""
-  echo "k8s-master-IP-address: IP address of k8s master node from which to get"
-  echo "          the debug data"
-  echo "username: optional username to login into the k8s master node. If no user"
-  echo "          name is specified, the current user will be used. The user must"
-  echo "          have passwordless access to the k8s master node and must be able"
-  echo "          to execute passwordless sudo."
-  echo ""
-  exit 1
+
+while getopts "h?f:m:u:" opt; do
+    case "$opt" in
+    h|\?)
+        usage
+        exit 0
+        ;;
+    f)  SSH_CONFIG_FILE=$OPTARG
+        ;;
+    u)  SSH_USER=$OPTARG
+        ;;
+    m)  MASTER=$OPTARG
+        ;;
+    esac
+done
+
+if [ -z "${MASTER+xxx}" ] ; then
+    echo "Error - Master node must be specified"
+    exit 1
 fi
 
-master=$1
-if [ "$num_args" -eq 2 ] ; then
-  user=$2
+if [ -z "${SSH_USER+xxx}" ] ; then
+    SSH_USER=$(whoami)
+elif [ "$SSH_USER" == "vagrant" ] ; then
+        if [ -z "${SSH_CONFIG_FILE+xxx}" ] ; then
+        echo "Error - ssh configuration file must be specified when using vagrant"
+    fi
+fi
+
+if [ -z "${SSH_CONFIG_FILE+xxx}" ]; then
+    SSH_OPTS=""
 else
-  user=$(whoami)
+    SSH_OPTS="-F $SSH_CONFIG_FILE"
 fi
 
-stamp="$(date "+%Y-%m-%d-%H-%M")"
-report_dir="contiv-vpp-bugreport-$stamp"
-mkdir -p "$report_dir"
+echo "$SSH_OPTS"
 
-vswitch_pods="$(ssh "$user"@"$master" kubectl get po -n kube-system -l k8s-app=contiv-vswitch -o go-template=\'{{range .items}}{{printf \"%s,%s \" \(index .metadata\).name \(index .spec\).nodeName}}{{end}}\')"
+
+STAMP="$(date "+%Y-%m-%d-%H-%M")"
+REPORT_DIR="contiv-vpp-bugreport-$STAMP"
+mkdir -p "$REPORT_DIR"
+
+vswitch_pods="$(ssh "$SSH_USER"@"$MASTER" $SSH_OPTS kubectl get po -n kube-system -l k8s-app=contiv-vswitch -o go-template=\'{{range .items}}{{printf \"%s,%s \" \(index .metadata\).name \(index .spec\).nodeName}}{{end}}\')"
+echo vswitch_pods $vswitch_pods
+
 for p in $vswitch_pods; do
   IFS=',' read -r -a fields <<< "$p"
   echo "Collecting logs for vswitch" \'"${fields[0]}"\'  "on node" \'"${fields[1]}"\'
-  mkdir "$report_dir"/"${fields[1]}"
-  ssh "$user"@"$master" kubectl logs "${fields[0]}" -n kube-system -c contiv-vswitch > "$report_dir"/"${fields[1]}"/"${fields[0]}".log
+  mkdir "$REPORT_DIR"/"${fields[1]}"
+  ssh "$SSH_USER"@"$MASTER" $SSH_OPTS kubectl logs "${fields[0]}" -n kube-system -c contiv-vswitch > "$REPORT_DIR"/"${fields[1]}"/"${fields[0]}".log
   set +e
-  ssh "$user"@"$master" kubectl logs "${fields[0]}" -n kube-system -c contiv-vswitch -p > "$report_dir"/"${fields[1]}"/"${fields[0]}"-previous.log 2>/dev/null
+  ssh "$SSH_USER"@"$MASTER" $SSH_OPTS kubectl logs "${fields[0]}" -n kube-system -c contiv-vswitch -p > "$REPORT_DIR"/"${fields[1]}"/"${fields[0]}"-previous.log 2>/dev/null
   if [ $? -ne 0 ]; then
-    rm "$report_dir"/"${fields[1]}"/"${fields[0]}"-previous.log
+    rm "$REPORT_DIR"/"${fields[1]}"/"${fields[0]}"-previous.log
   fi
   set -e
 done
@@ -57,24 +95,31 @@ done
 echo ""
 
 echo "Getting Kubernetes data:"
-echo " . configmaps"
-ssh "$user"@"$master" kubectl describe configmaps -n kube-system contiv-agent-cfg > "$report_dir"/vpp.yaml 2>/dev/null
-echo " . nodes"
-ssh "$user"@"$master" kubectl get nodes -o wide >  "$report_dir"/k8s-nodes.txt 2>/dev/null
-echo " . pods"
-ssh "$user"@"$master" kubectl get pods -o wide --all-namespaces > "$report_dir"/k8s-pods.txt 2>/dev/null
-echo " . services"
-ssh "$user"@"$master" kubectl get services -o wide --all-namespaces > "$report_dir"/k8s-services.txt 2>/dev/null
-echo " . networkpolicy"
-ssh "$user"@"$master" kubectl get networkpolicy -o wide --all-namespaces > "$report_dir"/k8s-policies.txt 2>/dev/null
+echo " - configmaps"
+ssh "$SSH_USER"@"$MASTER" $SSH_OPTS kubectl describe configmaps -n kube-system contiv-agent-cfg > "$REPORT_DIR"/vpp.yaml 2>/dev/null
+echo " - nodes"
+ssh "$SSH_USER"@"$MASTER" $SSH_OPTS kubectl get nodes -o wide >  "$REPORT_DIR"/k8s-nodes.txt 2>/dev/null
+echo " - pods"
+ssh "$SSH_USER"@"$MASTER" $SSH_OPTS kubectl get pods -o wide --all-namespaces > "$REPORT_DIR"/k8s-pods.txt 2>/dev/null
+echo " - services"
+ssh "$SSH_USER"@"$MASTER" $SSH_OPTS kubectl get services -o wide --all-namespaces > "$REPORT_DIR"/k8s-services.txt 2>/dev/null
+echo " - networkpolicy"
+ssh "$SSH_USER"@"$MASTER" $SSH_OPTS kubectl get networkpolicy -o wide --all-namespaces > "$REPORT_DIR"/k8s-policies.txt 2>/dev/null
 echo ""
 
-nodes="$(ssh "$user"@"$master" kubectl get nodes -o go-template=\'{{range .items}}{{printf \"%s,%s \" \(index .status.addresses 0\).address \(index .metadata\).name}}{{end}}\')"
+nodes="$(ssh "$SSH_USER"@"$MASTER" $SSH_OPTS kubectl get nodes -o go-template=\'{{range .items}}{{printf \"%s,%s \" \(index .status.addresses 0\).address \(index .metadata\).name}}{{end}}\')"
 for n in $nodes; do
   IFS=',' read -r -a fields <<< "$n"
-  echo Getting VPP information from \'"${fields[1]}"\':
-  ssh "$user"@"${fields[0]}" mkdir /tmp/"$report_dir"
+    if [ $SSH_USER == "vagrant" ] ; then
+    NODE="${fields[1]}"
+  else
+    NODE="${fields[0]}"
+  fi
 
+  echo Getting data from \'"${fields[1]}"\':
+  ssh "$SSH_USER"@"$NODE" $SSH_OPTS mkdir /tmp/"$REPORT_DIR"
+
+  echo " - VPP:"
   # get_vpp_data <target-vpp-command> <file-name-string> <console-message-string>
   get_vpp_data "sh int" interface
   get_vpp_data "sh int addr" interface-address
@@ -88,12 +133,16 @@ for n in $nodes; do
   get_vpp_data "sh hardware-interfaces" hardware-info
   get_vpp_data "sh errors" errors
 
-  ssh $user@${fields[0]} tar -cC /tmp/$report_dir/ . | tar -xC $report_dir/${fields[1]} 2>/dev/null
+  echo " - Logs for contiv-stn"
+  ssh -t $SSH_OPTS "$SSH_USER@$NODE" 'sudo docker logs $(sudo docker ps --filter name=contiv-stn --format "{{.ID}}")' \> /tmp/$REPORT_DIR/contiv-stn.log 2>/dev/null || true
+
+
+  ssh "$SSH_USER"@"$NODE" $SSH_OPTS tar -cC /tmp/"$REPORT_DIR"/ . | tar -xC "$REPORT_DIR"/${fields[1]} 2>/dev/null
 
   echo ""
 done
 
 echo "Creating tar file"
-tar -z -cvf $report_dir.tgz $report_dir >/dev/null
-# rm -rf $report_dir
+tar -z -cvf "$REPORT_DIR".tgz "$REPORT_DIR" >/dev/null
+rm -rf "$REPORT_DIR"
 echo "Done."

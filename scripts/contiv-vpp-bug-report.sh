36f71beee23532a8179343898ca2c25ca1a37a8c
Andrej Marcinek <amarcine@cisco.com>
2018-07-10 09:18:44 +0200

    changed work folder to tmp

--- scripts/contiv-vpp-bug-report.sh
+++ scripts/contiv-vpp-bug-report.sh
@@ -221,7 +221,7 @@ LOCAL_COMMANDS["linux-ip-route.log"]="ip route"
 LOCAL_COMMANDS["contiv-stn.log"]='CONTAINER=$(sudo docker ps --filter name=contiv-stn --format "{{.ID}}") && [ -n "$CONTAINER" ] && sudo docker logs "$CONTAINER"'
 LOCAL_COMMANDS["vswitch-version.log"]="curl -m 2 localhost:9999/liveness"
 LOCAL_COMMANDS["docker-ps.log"]="sudo docker ps"
-LOCAL_COMMANDS["core-dump.log"]='mkdir -p /vagrant/dumps && sudo tar -zcf /vagrant/dumps/`hostname`-dump.tgz /run/vpp/dumps && ls -alR /vagrant/dumps'
+LOCAL_COMMANDS["core-dump.log"]='mkdir -p /tmp/dumps && sudo tar -zcf /tmp/dumps/`hostname`-dump.tgz /run/vpp/dumps && ls -alR /tmp/dumps'
 
 declare -A ETCD_COMMANDS
 ETCD_COMMANDS["etcd-tree.log"]="export ETCDCTL_API=3 && etcdctl --endpoints=127.0.0.1:32379 get / --prefix=true"
@@ -393,7 +393,7 @@ then
             done
 
             RSYNC_SSH="ssh ${SSH_OPTS[@]}"
-            rsync --remove-source-files -e "$RSYNC_SSH" "$SSH_USER@$NODE_NAME:/vagrant/dumps/*" . || true </dev/null
+            rsync --remove-source-files -e "$RSYNC_SSH" "$SSH_USER@$NODE_NAME:/tmp/dumps/*" . || true </dev/null
             save_container_nw_report </dev/null
 
             echo
@@ -411,7 +411,7 @@ then
             get_shell_data_local "$CMD_INDEX" ${LOCAL_COMMANDS[$CMD_INDEX]}
         done
 
-        mv /vagrant/dumps/* . || true
+        mv /tmp/dumps/* . || true
         save_container_nw_report
 
         echo

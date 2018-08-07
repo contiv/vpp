653648356a56b18b9aac939d3ce2479acfcf6ba4
Jan Medved <jmedved@cisco.com>
2018-08-07 15:57:35 -0700

    Fix validator test nodieInfo data & small cache_impl bug

--- plugins/crd/cache/validator_test.go
+++ plugins/crd/cache/validator_test.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	nodemodel "github.com/contiv/vpp/plugins/ksr/model/node"
 	"github.com/ligato/cn-infra/logging"
 	"github.com/ligato/cn-infra/logging/logrus"
 	"github.com/onsi/gomega"
@@ -10,16 +11,19 @@ import (
 type validatorTestVars struct {
 	log       *logrus.Logger
 	processor *ContivTelemetryProcessor
+	logWriter *mockLogWriter
+
 
 	// Mock data
-	nodesData []nodeData
+	nodeInfoData []*nodeData
+	k8sNodeData  []*nodemodel.Node
 }
 
 type nodeData struct {
-	ID       uint32
-	nodeName string
-	IPAdr    string
-	ManIPAdr string
+	ID        uint32
+	nodeName  string
+	IPAdr     string
+	ManIPAdr  string
 
 	liveness   *NodeLiveness
 	interfaces nodeInterfaces
@@ -34,10 +38,13 @@ func TestValidator(t *testing.T) {
 	gomega.RegisterTestingT(t)
 
 	// Initialize & start mock objects
+	vtv.logWriter = &mockLogWriter{log: []string{}}
 	vtv.log = logrus.DefaultLogger()
-	vtv.log.SetLevel(logging.DebugLevel)
+	vtv.log.SetLevel(logging.ErrorLevel)
+	vtv.log.SetOutput(vtv.logWriter)
 
-	vtv.createTestData()
+	vtv.createNodeInfoTestData()
+	vtv.createK8sNodeTestData()
 
 	// Initialize the cache
 	vtv.processor = &ContivTelemetryProcessor{
@@ -54,8 +61,58 @@ func TestValidator(t *testing.T) {
 	vtv.processor.ContivTelemetryCache.Init()
 
 	// Populate cache data
-	cache := vtv.processor.ContivTelemetryCache
-	for _, node := range vtv.nodesData {
+
+	// Do the testing
+	t.Run("testErrorFreeTopologyValidation", testErrorFreeTopologyValidation)
+
+	t.Run("testK8sNodeToNodeInfoOkValidation", testK8sNodeToNodeInfoOkValidation)
+	t.Run("testK8sNodeToNodeInfoMissingNiValidation", testK8sNodeToNodeInfoMissingNiValidation)
+	t.Run("testK8sNodeToNodeInfoMissingK8snValidation", testK8sNodeToNodeInfoMissingK8snValidation)
+}
+
+func testErrorFreeTopologyValidation(t *testing.T) {
+	populateNodeInfoDataInCache(vtv.processor.ContivTelemetryCache)
+
+	vtv.processor.ValidateNodeInfo()
+
+	printErrorReport(vtv.processor.ContivTelemetryCache.Cache.report)
+}
+
+func testK8sNodeToNodeInfoOkValidation(t *testing.T) {
+	vtv.processor.ContivTelemetryCache.ReinitializeCache()
+	populateNodeInfoDataInCache(vtv.processor.ContivTelemetryCache)
+	populateK8sNodeDataInCache(vtv.processor.ContivTelemetryCache)
+
+	vtv.processor.ContivTelemetryCache.Cache.ValidateK8sNodeInfo()
+
+	printErrorReport(vtv.processor.ContivTelemetryCache.Cache.report)
+}
+
+func testK8sNodeToNodeInfoMissingNiValidation(t *testing.T) {
+	vtv.processor.ContivTelemetryCache.ReinitializeCache()
+	populateNodeInfoDataInCache(vtv.processor.ContivTelemetryCache)
+	populateK8sNodeDataInCache(vtv.processor.ContivTelemetryCache)
+	vtv.processor.ContivTelemetryCache.DeleteNode([]string{"k8s-master"})
+
+	vtv.processor.ContivTelemetryCache.Cache.ValidateK8sNodeInfo()
+
+	printErrorReport(vtv.processor.ContivTelemetryCache.Cache.report)
+}
+
+func testK8sNodeToNodeInfoMissingK8snValidation(t *testing.T) {
+	vtv.processor.ContivTelemetryCache.ReinitializeCache()
+	populateNodeInfoDataInCache(vtv.processor.ContivTelemetryCache)
+	populateK8sNodeDataInCache(vtv.processor.ContivTelemetryCache)
+	delete(vtv.processor.ContivTelemetryCache.Cache.k8sNodeMap, "k8s-master")
+
+	vtv.processor.ContivTelemetryCache.Cache.ValidateK8sNodeInfo()
+
+	printErrorReport(vtv.processor.ContivTelemetryCache.Cache.report)
+}
+
+
+func populateNodeInfoDataInCache(cache *ContivTelemetryCache) {
+	for _, node := range vtv.nodeInfoData {
 		cache.AddNode(node.ID, node.nodeName, node.IPAdr, node.ManIPAdr)
 		cache.Cache.SetNodeLiveness(node.nodeName, node.liveness)
 		cache.Cache.SetNodeInterfaces(node.nodeName, node.interfaces)
@@ -63,27 +120,23 @@ func TestValidator(t *testing.T) {
 		cache.Cache.SetNodeL2Fibs(node.nodeName, node.l2FibTable)
 		cache.Cache.SetNodeIPARPs(node.nodeName, node.arpTable)
 	}
-
-	// Do the testing
-	t.Run("testBasicOkTopology", testBasicOkTopology)
-
 }
 
-func testBasicOkTopology(t *testing.T) {
-	vtv.processor.ValidateNodeInfo()
-
-	printErrorReport(vtv.processor.ContivTelemetryCache.Cache.report)
+func populateK8sNodeDataInCache(cache *ContivTelemetryCache) {
+	for _, node := range vtv.k8sNodeData {
+		cache.AddK8sNode(node.Name, node.Pod_CIDR, node.Provider_ID, node.Addresses, node.NodeInfo)
+	}
 }
 
 // createNodeInfoOkTestData creates a test vector that roughly corresponds to a 3-node
 // vagrant topology (1 master, 2 workers). The created topology is defect-free,
 // i.e. defect must be injected into the topology individually for each test
 // case.
-func (v *validatorTestVars) createTestData() {
-	v.nodesData = []nodeData{}
+func (v *validatorTestVars) createNodeInfoTestData() {
+	v.nodeInfoData = []*nodeData{}
 
 	// Initialize k8s-master
-	k8sMaster := nodeData{
+	k8sMaster := &nodeData{
 		ID:       3,
 		nodeName: "k8s-master",
 		IPAdr:    "192.168.16.3/24",
@@ -165,11 +218,6 @@ func (v *validatorTestVars) createTestData() {
 		},
 		bds: map[int]NodeBridgeDomain{
 			1: {
-				Name:       "vxlanBD",
-				Forward:    true,
-				Interfaces: []bdinterfaces{},
-			},
-			2: {
 				Name:    "vxlanBD",
 				Forward: true,
 				Interfaces: []bdinterfaces{
@@ -227,10 +275,10 @@ func (v *validatorTestVars) createTestData() {
 			},
 		},
 	}
-	v.nodesData = append(v.nodesData, k8sMaster)
+	v.nodeInfoData = append(v.nodeInfoData, k8sMaster)
 
 	// Initialize k8s-worker1
-	k8sWorker1 := nodeData{
+	k8sWorker1 := &nodeData{
 		ID:       2,
 		nodeName: "k8s-worker1",
 		IPAdr:    "192.168.16.2/24",
@@ -272,9 +320,9 @@ func (v *validatorTestVars) createTestData() {
 				VppInternalName: "loop0",
 				Name:            "vxlanBVI",
 				Enabled:         true,
-				PhysAddress:     "1a:2b:3c:4d:5e:01",
+				PhysAddress:     "1a:2b:3c:4d:5e:02",
 				Mtu:             1500,
-				IPAddresses:     []string{"192.168.30.1/24"},
+				IPAddresses:     []string{"192.168.30.2/24"},
 			},
 			4: {
 				VppInternalName: "vxlan_tunnel0",
@@ -301,11 +349,6 @@ func (v *validatorTestVars) createTestData() {
 		},
 		bds: nodeBridgeDomains{
 			1: {
-				Name:       "vxlanBD",
-				Forward:    true,
-				Interfaces: []bdinterfaces{},
-			},
-			2: {
 				Name:    "vxlanBD",
 				Forward: true,
 				Interfaces: []bdinterfaces{
@@ -351,10 +394,10 @@ func (v *validatorTestVars) createTestData() {
 			},
 		},
 	}
-	v.nodesData = append(v.nodesData, k8sWorker1)
+	v.nodeInfoData = append(v.nodeInfoData, k8sWorker1)
 
 	// Initialize k8s-worker2
-	k8sWorker2 := nodeData{
+	k8sWorker2 := &nodeData{
 		ID:       1,
 		nodeName: "k8s-worker2",
 		IPAdr:    "192.168.16.1/24",
@@ -470,5 +513,76 @@ func (v *validatorTestVars) createTestData() {
 			},
 		},
 	}
-	v.nodesData = append(v.nodesData, k8sWorker2)
+	v.nodeInfoData = append(v.nodeInfoData, k8sWorker2)
+}
+
+func (v *validatorTestVars) createK8sNodeTestData() {
+	v.k8sNodeData = []*nodemodel.Node{}
+
+	k8sMaster := &nodemodel.Node{
+		Name:        "k8s-master",
+		Pod_CIDR:    "10.0.0.0/24",
+		Provider_ID: "",
+		Addresses: []*nodemodel.NodeAddress{
+			{Type: 3, Address: "10.20.0.2"},
+			{Type: 1, Address: "k8s-master"},
+		},
+		NodeInfo: &nodemodel.NodeSystemInfo{
+			Machine_ID:              "91550c3d3d1bca06c11d4f64575584db",
+			System_UUID:             "AC7BF39D-C7B5-4FB8-A2AD-32BD08DB8325",
+			Boot_ID:                 "be649475-5bf4-4f20-bb3c-7a98610d375a",
+			KernelVersion:           "4.4.0-21-generic",
+			OperatingSystem:         "Ubuntu 16.04 LTS",
+			ContainerRuntimeVersion: "docker://18.6.0",
+			KubeletVersion:          "v1.10.5",
+			OsImage:                 "linux",
+			Architecture:            "amd64",
+		},
+	}
+	v.k8sNodeData = append(v.k8sNodeData, k8sMaster)
+
+	k8sWorker1 := &nodemodel.Node{
+		Name:        "k8s-worker1",
+		Pod_CIDR:    "10.0.1.0/24",
+		Provider_ID: "",
+		Addresses: []*nodemodel.NodeAddress{
+			{Type: 3, Address: "10.20.0.10"},
+			{Type: 1, Address: "k8s-worker1"},
+		},
+		NodeInfo: &nodemodel.NodeSystemInfo{
+			Machine_ID:              "91550c3d3d1bca06c11d4f64575584db",
+			System_UUID:             "EF76A9B2-4AE5-4372-96EF-FF5B49C6EE99",
+			Boot_ID:                 "86e57d29-8525-48a0-a0a1-99cd06b415b2",
+			KernelVersion:           "4.4.0-21-generic",
+			OperatingSystem:         "Ubuntu 16.04 LTS",
+			ContainerRuntimeVersion: "docker://18.6.0",
+			KubeletVersion:          "v1.10.5",
+			OsImage:                 "linux",
+			Architecture:            "amd64",
+		},
+	}
+	v.k8sNodeData = append(v.k8sNodeData, k8sWorker1)
+
+	k8sWorker2 := &nodemodel.Node{
+		Name:        "k8s-worker2",
+		Pod_CIDR:    "10.0.7.0/24",
+		Provider_ID: "",
+		Addresses: []*nodemodel.NodeAddress{
+			{Type: 3, Address: "10.20.0.11"},
+			{Type: 1, Address: "k8s-worker2"},
+		},
+		NodeInfo: &nodemodel.NodeSystemInfo{
+			Machine_ID:              "91550c3d3d1bca06c11d4f64575584db",
+			System_UUID:             "E82E94E3-39C8-42A7-BD4D-9D8BDAF5BD59",
+			Boot_ID:                 "cd51dc78-b400-4a39-a144-ae1d1f7391a1",
+			KernelVersion:           "4.4.0-21-generic",
+			OperatingSystem:         "Ubuntu 16.04 LTS",
+			ContainerRuntimeVersion: "docker://18.6.0",
+			KubeletVersion:          "v1.10.5",
+			OsImage:                 "linux",
+			Architecture:            "amd64",
+		},
+	}
+	v.k8sNodeData = append(v.k8sNodeData, k8sWorker2)
+
 }

98e0d8471c61e027978bfcea62158319f61e429e
Jan Medved <jmedved@cisco.com>
2018-08-31 23:25:42 -0700

    C leanup l2 validator test and add l3 validator test

---
+++ plugins/crd/validator/l3/l3_validator_test.go
@@ -0,0 +1,75 @@
+// Copyright (c) 2018 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package l3
+
+import (
+	"fmt"
+	"github.com/contiv/vpp/plugins/crd/datastore"
+	nodemodel "github.com/contiv/vpp/plugins/ksr/model/node"
+	podmodel "github.com/contiv/vpp/plugins/ksr/model/pod"
+	"github.com/ligato/cn-infra/logging/logrus"
+	"strings"
+)
+
+type l3ValidatorTestVars struct {
+	log         *logrus.Logger
+	l3Validator *Validator
+	logWriter   *mockLogWriter
+
+	// Mock data
+	k8sNodeData []*nodemodel.Node
+	k8sPodData  []*podmodel.Pod
+
+	vppCache *datastore.VppDataStore
+	k8sCache *datastore.K8sDataStore
+	report   *datastore.SimpleReport
+}
+
+// mockLogWriter collects all error logs into a buffer for analysis
+// by gomega assertions.
+type mockLogWriter struct {
+	log []string
+}
+
+func (mlw *mockLogWriter) Write(p []byte) (n int, err error) {
+	logStr := string(p)
+	mlw.log = append(mlw.log, logStr)
+	return len(logStr), nil
+}
+
+func (mlw *mockLogWriter) clearLog() {
+	mlw.log = []string{}
+}
+
+func (mlw *mockLogWriter) printLog() {
+	fmt.Println("Error log:")
+	fmt.Println("==========")
+	for i, l := range mlw.log {
+		fmt.Printf("%d: %s", i, l)
+	}
+}
+
+func (mlw *mockLogWriter) countErrors() int {
+	cnt := 0
+	for _, logLine := range mlw.log {
+		if strings.Contains(logLine, "level=error") {
+			cnt++
+		}
+	}
+	return cnt
+}
+
+var vtv l3ValidatorTestVars

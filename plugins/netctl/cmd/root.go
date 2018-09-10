cfc54286dd29f7bbff306b12ae82a4c6a8d42baf
jmedved <jmedved@cisco.com>
2018-09-10 15:13:56 -0700

    Netctl refactor (#1100)
    * Refactor correlation of pod interfaces and pod-facing vpp interfaces
    Add more L2 Validator unit tests
    
    * Cleanup and add comments to l2 validator
    Add unit tests to L3 validator
    
    * Add more l3 validator tests
    
    * Cleanup and streamline l2 and l3 validator
    
    * Refactor netctl:
    - Put each command into its own file
    - Pull all commands into a common folder - cmdimpl
    - Move utilities used by all commands into a common file
    - Refactor pods command to only request node information (ipam and interfaces) once
    
    * Fix go fmt and golint issues
    
    * Fix go fmt issues
    
    * Fix nodestart time printout in the node command (int -> Unix date)
    
    * Cleanup output formatting for netctl commands
    
    * Improve output formatting of the netctl pods command
    
    * Fix go formatting issues
    
    * Enable printing ipam for multi-nodes with a single CLI command

--- plugins/netctl/cmd/root.go
+++ plugins/netctl/cmd/root.go
@@ -17,39 +17,40 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/contiv/vpp/plugins/netctl/nodes"
-	"github.com/contiv/vpp/plugins/netctl/vppdump"
+	"github.com/contiv/vpp/plugins/netctl/cmdimpl"
 	"github.com/spf13/cobra"
 	"os"
 )
 
 var cmdNodes = &cobra.Command{
 	Use:   "nodes",
-	Short: "Shows available nodes from contiv-ksr",
+	Short: "Shows vswitch information for all nodes in the running Contiv cluster.",
 	Args:  cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
-		nodes.PrintNodes()
+		cmdimpl.PrintNodes()
 	},
 }
 var cmdVppDump = &cobra.Command{
-	Use:   "vppdump nodename ",
-	Short: "Print anything to the screen",
-	Args:  cobra.MinimumNArgs(1),
+	Use:     "vppdump nodename ",
+	Short:   "Print anything to the screen",
+	Example: "netctl vppdump k8s-master",
+	Args:    cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		nodeName := args[0]
 		if len(args) == 2 {
 			vppDumpType := args[1]
-			vppdump.DumpCmd(nodeName, vppDumpType)
+			cmdimpl.DumpCmd(nodeName, vppDumpType)
 		} else {
-			vppdump.DumpCmd(nodeName, "")
+			cmdimpl.DumpCmd(nodeName, "")
 		}
 	},
 }
 
 var cmdVppCLI = &cobra.Command{
-	Use:   "vppcli nodename vpp-cli-command ",
-	Short: "Execute VPP debug CLI command on the specified node and print out the result",
-	Args:  cobra.MinimumNArgs(1),
+	Use:     "vppcli nodename vpp-cli-command",
+	Short:   "Execute the specified VPP debug CLI command on the specified node.",
+	Example: "netctl vppcli k8s-master sh int addr",
+	Args:    cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		nodeName := args[0]
 		if len(args) >= 2 {
@@ -57,7 +58,7 @@ var cmdVppCLI = &cobra.Command{
 			for _, str := range args[1:] {
 				vppCliCmd += str + " "
 			}
-			nodes.VppCliCmd(nodeName, vppCliCmd)
+			cmdimpl.VppCliCmd(nodeName, vppCliCmd)
 		} else if nodeName == "" {
 			fmt.Println("Enter a node name for vppcli: vppcli <nodeName> <cli_cmd>")
 		} else {
@@ -67,30 +68,33 @@ var cmdVppCLI = &cobra.Command{
 }
 
 var cmdNodeIPam = &cobra.Command{
-	Use:   "ipam nodename",
-	Short: "Display table for node ipam",
-	Args:  cobra.MaximumNArgs(1),
+	Use:     "ipam <nodename>",
+	Short:   "Shows IPAM information for specified node.",
+	Example: "netctl ipam k8s-master",
+	Args:    cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		nodeName := args[0]
 		if len(args) < 1 {
-			fmt.Println("\nEnter either a node name or an ip to display IPam information...")
+			cmdimpl.PrintAllIpams()
 		} else {
-			nodes.NodeIPamCmd(nodeName)
+			nodeName := args[0]
+			cmdimpl.NodeIPamCmd(nodeName)
 		}
 	},
 }
 
 var cmdPodInfo = &cobra.Command{
-	Use:   "pods nodename",
-	Short: "Display non-local pods for a given node or all if none is specified",
-	Args:  cobra.MaximumNArgs(1),
+	Use: "pods nodename",
+	Short: "Display network information for pods connected to VPP on the given node. If node is omitted, " +
+		"pod data for all nodes is shown.",
+	Example: "netctl pods k8-master\nnetctl pods",
+	Args:    cobra.MaximumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 
 		if len(args) < 1 {
-			nodes.PrintAllPods()
+			cmdimpl.PrintAllPods()
 		} else {
 			nodeName := args[0]
-			nodes.PrintPodsPerNode(nodeName)
+			cmdimpl.PrintPodsPerNode(nodeName)
 		}
 
 	},

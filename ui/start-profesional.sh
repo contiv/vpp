d0bf494f37f07582426c91d5bdfc2ba27af903ef
Lubomir Balogh <lubalogh@cisco.com>
2018-12-04 16:36:28 +0100

    Add startup scripts

--- ui/start-profesional.sh
+++ ui/start-profesional.sh
@@ -2,24 +2,16 @@
 echo "Starting Contiv VPP ..."
 echo ""
 
-cd ../vagrant
-sh ./vagrant-dummy-start
-
-echo "Setting Kubernetes proxy ..."
-echo ""
-vagrant ssh k8s-master -c "kubectl proxy --port=8080 &"
+cd ../vagrant && sh ./vagrant-start
 
 echo "Setting VMs port forwarding ..."
 
-cd ../ui
-sh ./setVMs.sh
+cd ../ui && sh ./setVMs.sh
 
 echo "Port forwarding has been set."
 
 echo "Starting vagrant ..."
-
-cd ./vagrant
-vagrant up
+cd ./vagrant && vagrant up
 
 echo ""
 echo "Application has been deployed. Open URL http://localhost:4200 in your browser with disabled security."

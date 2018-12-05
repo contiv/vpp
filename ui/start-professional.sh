647ba3bf1a09bf9eb16de5d6f48df57ef9a74097
Lubomir Balogh <lubalogh@cisco.com>
2018-12-05 15:20:33 +0100

    Start-up scripts - launch browser

--- ui/start-professional.sh
+++ ui/start-professional.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 echo "Starting Contiv VPP ..."
 echo ""
 
@@ -14,4 +15,20 @@ cd ./vagrant
 vagrant up
 
 echo ""
-echo "Application has been deployed. Open URL http://localhost:4300 in your browser with disabled security."
+echo "Application has been deployed on http://localhost:4300"
+echo ""
+
+case "$OSTYPE" in
+
+  darwin*)
+    Open -n -a "Google Chrome" --args --disable-web-security --user-data-dir=/tmp/chrome http://localhost:4300 || echo "Open URL http://localhost:4300 in your browser with disabled web security."
+    ;;
+
+  linux*)
+    google-chrome --user-data-dir=”/var/tmp/Chrome” --disable-web-security "http://localhost:4300" || echo "Open URL http://localhost:4300 in your browser with disabled web security."
+    ;;
+
+  *)
+    start chrome.exe --user-data-dir="C://Chrome dev session" --disable-web-security http://localhost:4300 || echo "Open URL http://localhost:4300 in your browser with disabled web security."
+    ;;
+esac

27276c02a25417f8bb9c745248b986504672b580
Giles Heron <giheron@cisco.com>
2018-12-05 17:06:02 -0800

    add service test - my_pod.py

---
+++ vagrant/my_pod.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+import time
+
+from kubernetes import config, client
+from kubernetes.client import Configuration
+from kubernetes.client.apis import core_v1_api
+from kubernetes.client.rest import ApiException
+from kubernetes.stream import stream
+
+class my_pod(object):
+	_api = ""
+	_name = "" 
+	_conn = ""
+
+	def __init__(self, name, image, host):
+		self._api = core_v1_api.CoreV1Api()
+		self._name = name + "-" + host
+		resp = None
+		try:
+			resp = self._api.read_namespaced_pod(name=self._name,
+							     namespace='default')
+		except ApiException as e:
+			if e.status != 404:
+				print("Unknown error: %s" %e)
+				exit(1)
+		if not resp:
+			print("Pod %s does not exist. Creating it..." % self._name)
+			pod_manifest = {
+ 				'apiVersion': 'v1',
+				'kind': 'Pod',
+				'metadata': {
+					'name': self._name
+				},
+			        'spec': {
+					'containers': [{
+						'image': image, 
+						'name': 'sleep',
+						"args": [
+							"/bin/sh",
+							"-c",
+							"while true;do date;sleep 5; done"
+						]
+					}],
+					'hostname': self._name,
+					'subdomain': 'default-subdomain',
+					'restartPolicy': 'Always',
+					'affinity': {
+						'nodeAffinity': {
+							'requiredDuringSchedulingIgnoredDuringExecution': {
+								'nodeSelectorTerms': [{
+									'matchExpressions': [{
+										'key': 'kubernetes.io/hostname',
+										'operator': 'In',
+										'values': [host] 
+									}]
+								}]
+							}
+
+						}
+					},
+					"tolerations": [{
+						'operator': 'Exists'
+					}]
+				}
+			}
+
+			print pod_manifest
+
+			resp = self._api.create_namespaced_pod(body=pod_manifest,
+                        				       namespace='default')
+    			while True:
+				resp = self._api.read_namespaced_pod(name=self._name,
+								     namespace='default')
+				if resp.status.phase != 'Pending':
+					break
+				time.sleep(1)
+			print("Done.")
+ 
+ 	def open_conn(self):
+		self._conn = stream(	self._api.connect_get_namespaced_pod_exec,
+					self._name,
+					'default',
+					command=['/bin/sh'],
+					stderr=True,
+					stdin=True,
+					stdout=True,
+					tty=False,
+					_preload_content=False)
+	
+ 	def close_conn(self):
+		self._conn.close()
+
+ 	def send(self, command):
+		self._conn.update(timeout=1)
+		self._conn.write_stdin(command + "\n")
+		while self._conn.is_open():
+			self._conn.update(timeout=1)
+                	if self._conn.peek_stdout():
+                        	print("%s" % self._conn.read_stdout())
+				break
+
+	def get_name(self):
+		return self._name
+
+	def get_address(self):
+                resp = None
+		try:
+                        resp = self._api.read_namespaced_pod_status(name=self._name,
+								    namespace='default')
+                except ApiException as e:
+                        if e.status != 404:
+                                print("Unknown error: %s" %e)
+                                exit(1)
+
+		return resp.status.pod_ip
+
+if __name__ == '__main__':
+	pod = []
+	config.load_kube_config()
+	c = Configuration()
+	c.assert_hostname = False
+	Configuration.set_default(c)
+
+	# list nodes
+	v = client.CoreV1Api()
+	hosts = v.list_node().items
+ 
+	# create pods
+	for i in range (0, len(hosts)):
+		pod.append(my_pod('busyboxplus', 'radial/busyboxplus:curl', hosts[i].metadata.name))
+		pod[i].open_conn()
+
+	# test pods
+	for i in range (0, len(hosts)):
+		print pod[i].get_name(), "(", pod[i].get_address(), ")"
+		for j in range (0, 10):
+			pod[i].send('wget -O - 10.96.1.1/server_addr 2>/dev/null')
+
+	# close pods
+	for i in range(0, len(hosts)):
+		pod[i].close_conn()

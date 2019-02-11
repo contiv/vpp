896c9f3f87bc8fbe0a18777caa9fe7c3e4f5b9fa
Rastislav Szabo <raszabo@cisco.com>
2019-02-11 10:12:36 +0100

    Finalized external IPAM code

--- plugins/service/nat44_test.go
+++ plugins/service/nat44_test.go
@@ -43,7 +43,7 @@ import (
 
 	"github.com/contiv/vpp/plugins/contivconf"
 	nodeconfigcrd "github.com/contiv/vpp/plugins/crd/pkg/apis/nodeconfig/v1"
-	"github.com/contiv/vpp/plugins/ipam/contivipam"
+	"github.com/contiv/vpp/plugins/ipam"
 	epmodel "github.com/contiv/vpp/plugins/ksr/model/endpoints"
 	podmodel "github.com/contiv/vpp/plugins/ksr/model/pod"
 	svcmodel "github.com/contiv/vpp/plugins/ksr/model/service"
@@ -198,7 +198,7 @@ type plugins struct {
 	serviceLabel *MockServiceLabel
 	nodeSync     *MockNodeSync
 	contivConf   *contivconf.ContivConf
-	ipam         *contivipam.IPAM
+	ipam         *ipam.IPAM
 	podManager   *MockPodManager
 	natPlugin    *MockNatPlugin
 	txnTracker   *localclient.TxnTracker
@@ -256,8 +256,8 @@ func initPlugins(testName string, config *contivconf.Config, localEndpointWeight
 	Expect(plugins.contivConf.Resync(resyncEv, resyncEv.KubeState, 1, nil)).To(BeNil())
 
 	// IPAM real plugin
-	plugins.ipam = &contivipam.IPAM{
-		Deps: contivipam.Deps{
+	plugins.ipam = &ipam.IPAM{
+		Deps: ipam.Deps{
 			PluginDeps: infra.PluginDeps{
 				Log: logging.ForPlugin("ipam"),
 			},

11e6582054613fae393e555c8f190724d5350f2a
samuel.elias <samelias@cisco.com>
2019-05-28 19:54:11 +0200

    update vagrant scripts to support kubernetes 1.14
    Signed-off-by: samuel.elias <samelias@cisco.com>

--- vagrant/provision/bootstrap_master.sh
+++ vagrant/provision/bootstrap_master.sh
@@ -86,9 +86,9 @@ fi
 split_k8s_version="$(cut -d "." -f 2 <<< "${k8s_version}")"
 if [ $split_k8s_version -gt 10 ] ; then
   if [ "${node_os_release}" == "16.04" ] ; then
-    sed -i '1s/.*/KUBELET_EXTRA_ARGS=--node-ip='"$KUBE_MASTER_IP"'/' /etc/default/kubelet
+    echo 'KUBELET_EXTRA_ARGS=--node-ip="$KUBE_MASTER_IP"' > /etc/default/kubelet
   else
-    sed -i '1s/.*/KUBELET_EXTRA_ARGS=--node-ip='"$KUBE_MASTER_IP"' --resolv-conf=\/run\/systemd\/resolve\/resolv.conf/' /etc/default/kubelet
+    echo 'KUBELET_EXTRA_ARGS=--node-ip="$KUBE_MASTER_IP" --resolv-conf=/run/systemd/resolve/resolv.conf' > /etc/default/kubelet
   fi
   systemctl daemon-reload
   systemctl restart kubelet
@@ -129,7 +129,7 @@ etcd:
     dataDir: /var/lib/etcd
 imageRepository: k8s.gcr.io
 kind: ClusterConfiguration
-kubernetesVersion: v1.13.0
+kubernetesVersion: v$k8s_version
 networking:
   dnsDomain: cluster.local
   podSubnet: "$pod_network_cidr"

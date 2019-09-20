d710ec3bdf1d99a753a22656fb045dbfc23c3045
AdelBouridah <33121554+AdelBouridah@users.noreply.github.com>
2019-09-20 12:59:14 +0200

    Copyright plus sfc multi nodes examples (#1676)
    * Adding Contributor To Cpyright Plus Adding some multi-nodes SFC Examples
    
    Signed-off-by: Adel Bouridah <a.bouridah@centre-univ-mila.dz>

--- plugins/ipam/vnialloc/key.go
+++ plugins/ipam/vnialloc/key.go
@@ -1,4 +1,5 @@
 // Copyright (c) 2018 Cisco and/or its affiliates.
+// Other Contributors: Adel Bouridah ESIEE Paris, adel.boruidah@esiee.fr
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.

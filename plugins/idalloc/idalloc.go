b86d878f5382d5aedca99ef1f6b5c71aa8ea3438
Rastislav Szabo <raszabo@cisco.com>
2019-10-11 02:14:10 +0200

    Fix ID release logic in idalloc plugin
    Signed-off-by: Rastislav Szabo <raszabo@cisco.com>

--- plugins/idalloc/idalloc.go
+++ plugins/idalloc/idalloc.go
@@ -51,8 +51,9 @@ type IDAllocator struct {
 type Deps struct {
 	infra.PluginDeps
 
-	ContivConf contivconf.API
-	RemoteDB   nodesync.KVDBWithAtomic
+	ServiceLabel servicelabel.ReaderAPI
+	ContivConf   contivconf.API
+	RemoteDB     nodesync.KVDBWithAtomic
 }
 
 // poolMetadata contains metadata of a pool used for faster ID allocation.
@@ -153,7 +154,7 @@ func (a *IDAllocator) InitPool(name string, poolRange *idallocation.AllocationPo
 	pool := &idallocation.AllocationPool{
 		Name:          name,
 		Range:         poolRange,
-		IdAllocations: map[string]uint32{},
+		IdAllocations: map[string]*idallocation.AllocationPool_Allocation{},
 	}
 
 	// save the pool in db
@@ -252,7 +253,7 @@ func (a *IDAllocator) ReleaseID(poolName string, idLabel string) (err error) {
 	if poolMeta == nil {
 		return
 	}
-	id := pool.IdAllocations[idLabel]
+	alloc := pool.IdAllocations[idLabel]
 
 	succeeded := false
 	for i := 0; i < maxIDAllocationAttempts; i++ {
@@ -262,7 +263,7 @@ func (a *IDAllocator) ReleaseID(poolName string, idLabel string) (err error) {
 		}
 		if succeeded {
 			// successfully released an ID
-			delete(poolMeta.allocatedIDs, id)
+			delete(poolMeta.allocatedIDs, alloc.Id)
 			break
 		} else {
 			// pool changed in db, re-read from db and retry
@@ -281,7 +282,7 @@ func (a *IDAllocator) ReleaseID(poolName string, idLabel string) (err error) {
 		a.Log.Errorf("Error by releasing ID: %v", err)
 	}
 
-	a.Log.Debugf("Released ID for label '%s' in pool %s: %d", idLabel, poolName, id)
+	a.Log.Debugf("Released ID for label '%s' in pool %s: %d", idLabel, poolName, alloc.Id)
 
 	return nil
 }
@@ -291,8 +292,8 @@ func (a *IDAllocator) tryToAllocateID(pool *idallocation.AllocationPool, poolMet
 	id uint32, succeeded bool, err error) {
 
 	// step 0, try to get already allocated ID number
-	if id, exists := pool.IdAllocations[idLabel]; exists {
-		return id, true, nil
+	if alloc, exists := pool.IdAllocations[idLabel]; exists {
+		return alloc.Id, true, nil
 	}
 
 	// step 1, find a free ID number
@@ -316,7 +317,10 @@ func (a *IDAllocator) tryToAllocateID(pool *idallocation.AllocationPool, poolMet
 	if err != nil {
 		return 0, false, err
 	}
-	pool.IdAllocations[idLabel] = id
+	pool.IdAllocations[idLabel] = &idallocation.AllocationPool_Allocation{
+		Id:    id,
+		Owner: a.ServiceLabel.GetAgentLabel(),
+	}
 	newData, err := a.serializer.Marshal(pool)
 	if err != nil {
 		return 0, false, err
@@ -335,8 +339,17 @@ func (a *IDAllocator) tryToAllocateID(pool *idallocation.AllocationPool, poolMet
 func (a *IDAllocator) tryToReleaseID(pool *idallocation.AllocationPool, poolMeta *poolMetadata, idLabel string) (
 	succeeded bool, err error) {
 
-	// check if it is not already released
-	if _, exists := pool.IdAllocations[idLabel]; !exists {
+	alloc, exists := pool.IdAllocations[idLabel]
+	if !exists {
+		// already released
+		return true, nil
+	}
+
+	// delete the allocation from cache
+	delete(pool.IdAllocations, idLabel)
+
+	if alloc.Owner != a.ServiceLabel.GetAgentLabel() {
+		// we do not own this allocation, do not write into db
 		return true, nil
 	}
 
@@ -345,7 +358,6 @@ func (a *IDAllocator) tryToReleaseID(pool *idallocation.AllocationPool, poolMeta
 	if err != nil {
 		return false, err
 	}
-	delete(pool.IdAllocations, idLabel)
 	newData, err := a.serializer.Marshal(pool)
 	if err != nil {
 		return false, err
@@ -392,8 +404,8 @@ func (a *IDAllocator) buildPoolMetadata(pool *idallocation.AllocationPool) *pool
 	for _, id := range pool.Range.Reserved {
 		meta.reservedIDs[id] = true
 	}
-	for label, id := range pool.IdAllocations {
-		meta.allocatedIDs[id] = label
+	for label, alloc := range pool.IdAllocations {
+		meta.allocatedIDs[alloc.Id] = label
 	}
 	return meta
 }

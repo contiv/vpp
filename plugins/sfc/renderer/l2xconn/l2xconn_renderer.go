b7121aad1ac81d62a8074d5c1e3e661745a0003f
Adel Bouridah <a.bouridah@centre-univ-mila.dz>
2019-10-28 10:44:05 +0100

    Bug correction:Integrating milanlenco review
    Signed-off-by: Adel Bouridah <a.bouridah@centre-univ-mila.dz>

--- plugins/sfc/renderer/l2xconn/l2xconn_renderer.go
+++ plugins/sfc/renderer/l2xconn/l2xconn_renderer.go
@@ -138,8 +138,8 @@ func (rndr *Renderer) renderChain(sfc *renderer.ContivSFC, isDelete bool) (confi
 		}
 
 		if iface != "" && prevIface != "" {
-			if rndr.isNodeLocalSF(sf) && rndr.isNodeLocalSF(prevSF) {
-				// the two SFs (prevSF and SF) are local - Rgarding the node ID (lower or note) cross-connect the interfaces
+			if rndr.shouldChainLocalSFs(sf, prevSF) {
+				// the two SFs (prevSF and SF) are local and should be chained -  cross-connect the interfaces
 				if rndr.shouldChainLocalSFs(sf, prevSF) {
 					xconnect := rndr.crossConnectIfaces(prevIface, iface, sfc.Unidirectional)
 					rndr.mergeConfiguration(config, xconnect)
